e4293e89861366aec78ba64291d86650d382f3d3
landerv <lander.vanroye@kuleuven.be>
2021-11-24 11:24:27 +0100

    wip

--- examples/OCPExample.cpp
+++ examples/OCPExample.cpp
@@ -5,18 +5,37 @@
 #include "string"
 using namespace std;
 using namespace fatrop;
-int main(){
+int main()
+{
     // problem dimensions
     OCP_dims dims;
-    dims.K = 150;
-    dims.nx = vector<int>(dims.K, 12); 
-    dims.nu = vector<int>(dims.K, 3); 
-    dims.ng = vector<int>(dims.K, 0); 
+    dims.K = 150000;
+    int nx = 12;
+    int nu = 3;
+    dims.nx = vector<int>(dims.K, nx);
+    dims.nu = vector<int>(dims.K, nu);
+    dims.ng = vector<int>(dims.K, 0);
     // memory allocation
     fatrop_memory_allocator fma;
     OCP_KKT KKT(dims, fma);
     fma.allocate();
     random_OCP(KKT, dims, 0);
     // Sparse_OCP(dims, KKT).print("matrix");
+    blasfeo_dmat *RSQrq = (blasfeo_dmat *)KKT.RSQrqt[0];
+    blasfeo_dmat *BAbt = (blasfeo_dmat *)KKT.BAbt[0];
+    blasfeo_timer timer;
+    blasfeo_tic(&timer);
+    for (unsigned long int iter = 0; iter < 1e0; ++iter)
+    {
+        for (int i = 0; i < dims.K; ++i)
+        {
+            // blasfeo_dgese(nu + nx + 1, nu + nx, (double) i, RSQrq + i, 0, 0);
+            // blasfeo_dgese(nu + nx + 1, nx, (double) i, BAbt +i , 0, 0);
+            blasfeo_dgese(nu + nx + 1, nu + nx, (double) i, RSQrq , 0, 0);
+            blasfeo_dgese(nu + nx + 1, nx, (double) i, BAbt  , 0, 0);
+        }
+    }
+    double el = blasfeo_toc(&timer);
+    cout << "el time " << el << " seconds \n";
     return 0;
 }
--- include/FatropOCPKKT.hpp
+++ include/FatropOCPKKT.hpp
@@ -20,7 +20,7 @@ namespace fatrop
     class OCP_KKT
     {
     public:
-        OCP_KKT(const OCP_dims &dims, fatrop_memory_allocator &fma) : K(dims.K), nu(dims.K, vector<int>(dims.nu), fma), nx(dims.K, vector<int>(dims.nx), fma), ng(dims.K, vector<int>(dims.ng), fma), RSQrqt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.nu + dims.nx), dims.K, fma), BAbt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.nx), dims.K, fma), Ggt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.ng), dims.K, fma){};
+        OCP_KKT(const OCP_dims &dims, fatrop_memory_allocator &fma) : aux(dims, fma), K(dims.K), nu(dims.K, vector<int>(dims.nu), fma), nx(dims.K, vector<int>(dims.nx), fma), ng(dims.K, vector<int>(dims.ng), fma), RSQrqt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.nu + dims.nx), dims.K, fma), BAbt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.nx), dims.K, fma), Ggt(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.ng), dims.K, fma){};
         int K;
         fatrop_memory_el<int> nu;
         fatrop_memory_el<int> nx;
@@ -31,40 +31,66 @@ namespace fatrop
         fatrop_memory_matrix_bf BAbt;
         /// small-scale Jacobian stagewise eq constraints
         fatrop_memory_matrix_bf Ggt;
-    };
-    class OCP_aux
-    {
-    public:
-        OCP_aux(const OCP_dims &dims, fatrop_memory_allocator &fma) : ux_offs(dims.K, csum(dims.nx + dims.nu), fma), g_offs(dims.K, csum(dims.ng), fma), max_nu(max(dims.nu)), max_nx(max(dims.nx)), max_ng(max(dims.ng)){};
-        /// offset arrays are used for efficiency
-        fatrop_memory_el<int> ux_offs;
-        /// offset arrays are used for efficiency
-        fatrop_memory_el<int> g_offs;
-        int max_nu;
-        int max_nx;
-        int max_ng;
+        /** \brief this subclass contains some data that is cached for efficiency */
+        class OCP_aux
+        {
+        public:
+            OCP_aux(const OCP_dims &dims, fatrop_memory_allocator &fma) : ux_offs(dims.K, csum(dims.nx + dims.nu), fma), g_offs(dims.K, csum(dims.ng), fma), max_nu(max(dims.nu)), max_nx(max(dims.nx)), max_ng(max(dims.ng)){};
+            /// offset arrays are used for efficiency
+            fatrop_memory_el<int> ux_offs;
+            /// offset arrays are used for efficiency
+            fatrop_memory_el<int> g_offs;
+            int max_nu;
+            int max_nx;
+            int max_ng;
+        };
+        OCP_aux aux;
     };
     class OCP_KKT_solver
     {
     public:
-        OCP_KKT_solver(const OCP_dims &dims, fatrop_memory_allocator &fma) : aux(dims, fma),
+        OCP_KKT_solver(const OCP_dims &dims, fatrop_memory_allocator &fma) : Ppt(dims.nx + 1, vector<int>(dims.nx), dims.K, fma),
                                                                              Hht(vector<int>(dims.nu + dims.nx + 1), vector<int>(dims.ng), dims.K, fma),
-                                                                             Ppt(dims.nx + 1, vector<int>(dims.nx), dims.K, fma),
                                                                              gamma(dims.K, vector<int>(dims.K, 0), fma),
                                                                              rho(dims.K, vector<int>(dims.K, 0), fma){};
         // solve a KKT system
         void fact_solve(OCP_KKT *OCP, VEC *ux, VEC *lambda)
         {
+            // define compiler macros for notational convenience
+#define OCPMACRO(type, name, subfix) type name##subfix = ((type)OCP->name)
+#define AUXMACRO(type, name, subfix) type name##subfix = ((type)OCP->aux.name)
+#define SOLVERMACRO(type, name, subfix) type name##subfix = ((type)name)
             int K = OCP->K;
+            // make variables local for efficiency
+            OCPMACRO(MAT *, RSQrqt, _p);
+            OCPMACRO(MAT *, BAbt, _p);
+            OCPMACRO(MAT *, Ggt, _p);
+            SOLVERMACRO(MAT *, Ppt, _p);
+            SOLVERMACRO(MAT *, Hht, _p);
+            AUXMACRO(int, max_nu, );
+            AUXMACRO(int, max_nx, );
+            AUXMACRO(int, max_ng, );
+            AUXMACRO(int *, ux_offs, _p);
+            AUXMACRO(int *, g_offs, _p);
+            OCPMACRO(int *, nu, _p);
+            OCPMACRO(int *, nx, _p);
+            OCPMACRO(int *, ng, _p);
             // recursion
             {
                 // last stage
-                int nx = ((int *)OCP->nx)[K - 1];
-                // Hh_Km1 <- Gg_Km1
+                int nx = nx_p[K - 1];
+                int nu = nu_p[K - 1]; // this should be zero but is included here in case of misuse
+                int ng = ng_p[K - 1]; // this should be zero but is included here in case of misuse
                 // Pp_Km1 <- Qq_Km1
+                GECP(nx + 1, nx, RSQrqt_p + (K - 1), nu, nu, Ppt_p + K - 1, 0, 0);
+                // Hh_Km1 <- Gg_Km1
+                GECP(nx + 1, nx, RSQrqt_p + (K - 1), nu, nu, Hht_p, 0, 0);
             }
             for (int k = K - 2; k >= 0; --k)
             {
+                int nu = nu_p[k];
+                int nx = nx_p[k];
+                int ng = ng_p[k];
                 goto SUBSDYN;
             SUBSDYN:
                 // AL <- [BAb]^T_k P_kp1
@@ -82,7 +108,7 @@ namespace fatrop
                 // Ggt_tilde_k <- Ggt_stripe[rho_k:nu+nx+1, :rho] L-T (note that this is slightly different from the implementation)
                 // permutations
                 // Hh_k <- Ggt_stripe[nu:nu+nx+1, rho:] (transfer to next stage)
-                // GL <- Ggt_tilde_k @ RSQrqt[:, :rho]^T + RSQrqt[rho:nu+nx+1, rho:]^T (note the transpose of the last term!!) 
+                // GL <- Ggt_tilde_k @ RSQrqt[:, :rho]^T + RSQrqt[rho:nu+nx+1, rho:]^T (note the transpose of the last term!!)
                 // RSQrqt_hat = Gt_tilde_k[:-1,:] @ GL[:, :rho]^T + GL[:rho:]^T (note the transpose of the last term!!)
             SCHUR:
                 // DLlt_k = [chol(R_hatk)  Llk@chol(R_hatk)^-T]
@@ -91,8 +117,8 @@ namespace fatrop
                 // if gamma_0 - rho_0 > 0
                 // LU_FACT_TRANSPOSE(Hh0)
                 // permutations
-                // Ggt_tilde_I <- Ggt_stripe[rho_I:nx+1,:rho_I] L^-T 
-                // h_tilde_I <- - U_I ^-1 Ggt_tilde_I[nx+1, :] 
+                // Ggt_tilde_I <- Ggt_stripe[rho_I:nx+1,:rho_I] L^-T
+                // h_tilde_I <- - U_I ^-1 Ggt_tilde_I[nx+1, :]
                 // ?? if nx - rho_I > 0 ??
                 // GL_I <- Ggt_tildeI  @ Ppt_I^T + Ppt[rho:nx+1, rho:]^T
                 // Pphat_I <- Ggt_tilde_I[:-1,:] @  GL_I[:,:rho]^T + GL[:rho,:]^T
@@ -101,9 +127,8 @@ namespace fatrop
                 cout << "test" << endl;
             }
         }
-        OCP_aux aux;
-        fatrop_memory_matrix_bf Hht;
         fatrop_memory_matrix_bf Ppt;
+        fatrop_memory_matrix_bf Hht;
         fatrop_memory_el<int> gamma;
         fatrop_memory_el<int> rho;
     };
