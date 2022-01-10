c252195cdae70bcc27910790916059122a14ea66
landerv <lander.vanroye@kuleuven.be>
2022-01-10 10:22:20 +0100

    wip

---
+++ include/OCP/FatropOCPDims.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file FatropOCP.hpp
+ * @author your name (you@domain.com)
+ * @brief this file contains necessary the OCP specific code 
+ * @version 0.1
+ * @date 2021-11-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef FATROP_OCPDIMS_INCLUDED
+#define FATROP_OCPDIMS_INCLUDED
+#include "../AUX/FatropMemory.hpp"
+#include "../AUX/FatropVector.hpp"
+#include <vector>
+using namespace std;
+namespace fatrop
+{
+    /** \brief  this class contains the problem dimensions of a standard ocp*/
+    struct OCP_dims
+    {
+    public:
+        /// horizon length
+        int K;
+        /// input vector size
+        FatropVector<int> nu;
+        /// state vector size
+        FatropVector<int> nx;
+        // number of stagewise equality constraints
+        FatropVector<int> ng;
+    };
+} // namespace fatrop
+#endif //FATROP_OCPDIMS_INCLUDED
