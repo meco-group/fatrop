56029073b7d16279d42bf18b3c138a9b4f5bb133
landerv <lander.vanroye@kuleuven.be>
2022-01-25 12:14:04 +0100

    evalcache

--- include/SOLVER/FatropData.hpp
+++ include/SOLVER/FatropData.hpp
@@ -31,21 +31,40 @@ namespace fatrop
         {
             axpy(alpha_primal, delta_x, x_curr, x_next);
             axpby(alpha_primal, lam_calc, 1 - alpha_primal, lam_curr, lam_next);
+            // reset evaluation flags
+            evalcachenext = EvalCache();
             return 0;
         }
         int TakeStep()
         {
             x_curr.SwapWith(x_next);
             lam_curr.SwapWith(lam_next);
+            evalcachecurr = evalcachenext;
             return 0;
         }
         double CVLinfCurr()
         {
+            if(evalcachecurr.CVLinfEvaluated) return evalcachecurr.CVLinf;
+            evalcachecurr.CVLinfEvaluated = true;
             return Linf(g_curr);
         }
         double CVLinfNext()
         {
+            if(evalcachenext.CVLinfEvaluated) return evalcachenext.CVLinf;
+            evalcachenext.CVLinfEvaluated = true;
             return Linf(g_next);
+        }
+        double CVL1Curr()
+        {
+            if(evalcachecurr.CVL1Evaluated) return evalcachecurr.CVL1;
+            evalcachecurr.CVL1Evaluated = true;
+            return L1(g_curr);
+        }
+        double CVL1Next()
+        {
+            if(evalcachenext.CVL1Evaluated) return evalcachenext.CVL1;
+            evalcachenext.CVL1Evaluated = true;
+            return L1(g_next);
         }
         const NLPDims nlpdims;
         double obj_scale = 1.0;
@@ -63,6 +82,15 @@ namespace fatrop
         FatropVecBF g_next;
         FatropVecBF grad_curr;
         FatropVecBF grad_next;
+        struct EvalCache
+        {
+            bool CVLinfEvaluated = false;
+            double CVLinf = 0.0;
+            bool CVL1Evaluated = false;
+            double CVL1 = 0.0;
+        };
+        EvalCache evalcachecurr; 
+        EvalCache evalcachenext; 
     };
 }
 #endif // FATROPDATAINCLUDED
