c4b32f386e992916c7c3448f8f84384a3a36d2f9
Wilm Decre <wilm.decre@kuleuven.be>
2022-02-21 23:29:41 +0100

    wip

--- fatrop/aux/Aux.hpp
+++ fatrop/aux/Aux.hpp
@@ -8,21 +8,23 @@ namespace fatrop
     template <typename T, typename E>
     FatropVector<T> offsets(const VecExpr<E, T> &a)
     {
-        FatropVector<T> res(a.size());
+        const int size_a = a.size();
+        FatropVector<T> res(size_a);
         res.at(0) = 0;
-        for (int i = 1; i < a.size(); i++)
+        for (int i = 1; i < size_a; i++)
         {
             res.at(i) = a.getEl(i - 1) + res.getEl(i - 1);
         }
         return res;
     }
 
-    /** \brief returns index of max el of VecExpr*/
+    /** \brief returns index of max el of VecExpr */
     template <typename T, typename E>
     int max(const VecExpr<E, T> &a)
     {
+        const int size_a = a.size();
         int res = 0;
-        for (int i = 0; i < a.size(); i++)
+        for (int i = 0; i < size_a; i++)
         {
             int ai = a.getEl(i);
             res = ai > res ? ai : res;
