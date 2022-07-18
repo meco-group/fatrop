9719fbf7c6c34a0a83fb387208172a86a6d4f195
Wilm Decre <wilm.decre@kuleuven.be>
2022-07-18 11:45:01 +0200

    wip

--- cython/setup.py
+++ cython/setup.py
@@ -1,6 +1,17 @@
-from setuptools import setup
+from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
+fatrop_extension = Extension(
+    name="fatropy",
+    sources=["fatropy.pyx"],
+    libraries=["fatrop"],
+    library_dirs=["../build/fatrop"],
+    include_dirs=["../fatrop/ocp","../fatrop"],
+    language="c++",
+    define_macros=[("LEVEL1_DCACHE_LINE_SIZE","8")]
+)
 setup(
-    #ext_modules = cythonize(["..."])
+    name="fatropy",
+    ext_modules=cythonize([fatrop_extension])
 )
