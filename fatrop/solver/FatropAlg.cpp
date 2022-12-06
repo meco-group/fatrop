bfeaf243e54441e3b4d132b87debabb88ea61533
landerv <lander.vanroye@kuleuven.be>
2022-12-06 17:22:15 +0100

    change sd parameter

--- fatrop/solver/FatropAlg.cpp
+++ fatrop/solver/FatropAlg.cpp
@@ -216,7 +216,7 @@ int FatropAlg::Optimize()
             delta_w_last = deltaw;
         }
         double stepsize = std::max(LinfScaled(fatropdata_->delta_x, fatropdata_->x_curr), LinfScaled(fatropdata_->delta_s, fatropdata_->s_curr));
-        bool small_search_direction_curr = stepsize < 1e-10;
+        bool small_search_direction_curr = stepsize < 1e-14;
         lsinfo = linesearch_->FindAcceptableTrialPoint(mu, small_search_direction_curr || watch_dog_step, watch_dog_step);
         fatropdata_->RelaxBoundsVar(mu);
         fatropdata_->AdaptDualBounds(mu);
