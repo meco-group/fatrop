f0369971f644c8c35a6733587f5f00aee07e7a8a
landerv <lander.vanroye@kuleuven.be>
2022-12-19 23:45:09 +0100

    sampler in example

--- fatrop/executables/RunFatrop.cpp
+++ fatrop/executables/RunFatrop.cpp
@@ -5,33 +5,18 @@
 using namespace fatrop;
 int main(int argc, char **argv)
 {
-    // if (argc == 3)
-    // {
-    // string funcs = "/home/lander/projects/fatrop_motivation_examples/foobar/casadi_codegen.so";
-    // string jsons = "/home/lander/projects/fatrop_motivation_examples/foobar/casadi_codegen.json";
-    string funcs = "/home/lander/projects/fatrop-invariants-tutorial/foobar/casadi_codegen.so";
-    string jsons = "/home/lander/projects/fatrop-invariants-tutorial/foobar/casadi_codegen.json";
-    // OCPBuilder ocpbuilder(argv[1], argv[2]);
-    OCPBuilder ocpbuilder(funcs, jsons);
-    shared_ptr<FatropApplication> solver = ocpbuilder.Build();
-    solver->Optimize();
-    double target[] = {-1., 1., 1.};
-    ocpbuilder.GetParameterSetter("target_pos")->SetValue(target);
-    solver->Optimize();
-    auto sampler = ocpbuilder.GetSampler("state_x2");
-    vector<double> res(sampler -> Size());
-    blasfeo_timer timer;
-    blasfeo_tic(&timer);
-    sampler -> Sample(res);
-    double el = blasfeo_toc(&timer);
-    for (auto resi : res)
+    if (argc == 3)
     {
-        cout << resi << endl;
+        OCPBuilder ocpbuilder(argv[1], argv[2]);
+        // usage of parameter setter
+        // ocpbuilder.GetParameterSetter("target_pos")->SetValue({1., 2., 3.});
+        shared_ptr<FatropApplication> solver = ocpbuilder.Build();
+        solver->Optimize();
+        // usage of parameter sampler
+        // auto res = ocpbuilder.GetSampler("state_pos") -> Sample();
     }
-    cout << "sample time " << el << endl;
-    // }
-    // else
-    // {
-    //     cout << "run me as RunFatrop f.so f.json" << endl;
-    // }
+    else
+    {
+        cout << "run me as RunFatrop f.so f.json" << endl;
+    }
 }
