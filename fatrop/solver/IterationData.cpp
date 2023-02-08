ec0b80f903f62fcfe5644194ec96793148fda5c1
landerv <lander.vanroye@kuleuven.be>
2023-02-08 18:04:17 +0100

    concise printing

--- fatrop/solver/IterationData.cpp
+++ fatrop/solver/IterationData.cpp
@@ -8,7 +8,7 @@ void Journaller::PrintIterations()
 {
     if (print_count == 0)
     {
-        printf(" it  obj                    cv                  du                  lg(mu) reg  alpha_du  alpha_pr  ls\n");
+        printf(" it  obj            cv        du        lg(mu) reg  alpha_du  alpha_pr  ls\n");
     }
     for (std::vector<double>::size_type i = print_count; i < iterationdata.size(); i++)
     {
@@ -23,11 +23,11 @@ void Journaller::PrintIterations()
         // }
         if (iterationdata_i.reg == 0.0)
         {
-            printf("%3d, %.15e, %.12e, %.12e, %4.1f,  -.-, %.2e, %.2e, %d%c \n", iterationdata_i.iter, iterationdata_i.objective, iterationdata_i.constraint_violation, iterationdata_i.du_inf, log10(iterationdata_i.mu), iterationdata_i.alpha_du, iterationdata_i.alpha_pr, abs(iterationdata_i.ls), iterationdata_i.type);
+            printf("%3d, %.7e, %.2e, %.2e, %4.1f,  -.-, %.2e, %.2e, %d%c \n", iterationdata_i.iter, iterationdata_i.objective, iterationdata_i.constraint_violation, iterationdata_i.du_inf, log10(iterationdata_i.mu), iterationdata_i.alpha_du, iterationdata_i.alpha_pr, abs(iterationdata_i.ls), iterationdata_i.type);
         }
         else
         {
-            printf("%3d, %.15e, %.12e, %.12e, %4.1f, %4.1f, %.2e, %.2e, %d%c \n", iterationdata_i.iter, iterationdata_i.objective, iterationdata_i.constraint_violation, iterationdata_i.du_inf, log10(iterationdata_i.mu), log10(iterationdata_i.reg), iterationdata_i.alpha_du, iterationdata_i.alpha_pr, abs(iterationdata_i.ls), iterationdata_i.type);
+            printf("%3d, %.7e, %.2e, %.2e, %4.1f, %4.1f, %.2e, %.2e, %d%c \n", iterationdata_i.iter, iterationdata_i.objective, iterationdata_i.constraint_violation, iterationdata_i.du_inf, log10(iterationdata_i.mu), log10(iterationdata_i.reg), iterationdata_i.alpha_du, iterationdata_i.alpha_pr, abs(iterationdata_i.ls), iterationdata_i.type);
         }
     }
     print_count = iterationdata.size();
