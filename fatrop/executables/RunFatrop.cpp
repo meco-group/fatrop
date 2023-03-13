fb70ff3e7926bcfd6588f1f93c99f2bdff5d5883
landerv <lander.vanroye@kuleuven.be>
2023-03-13 13:12:50 +0100

    runfatrop cleanup

--- fatrop/executables/RunFatrop.cpp
+++ fatrop/executables/RunFatrop.cpp
@@ -6,23 +6,14 @@
 using namespace fatrop;
 int main(int argc, char **argv)
 {
-    // if (argc == 3)
-    // {
-        // OCPBuilder ocpbuilder(argv[1], argv[2]);
-        shared_ptr<BasicOCPApplication> app = BasicOCPApplicationBuilder::FromRockitInterface("./casadi_codegen.so", "./casadi_codegen.json");
-        // shared_ptr<OCPBuilder> app =make_shared<OCPBuilder>("./casadi_codegen.so", "./casadi_codegen.json");
-        cout << "Building the solver " << endl;
+    if (argc == 3)
+    {
+        shared_ptr<BasicOCPApplication> app = BasicOCPApplicationBuilder::FromRockitInterface(argv[1], argv[2]);
         app -> Build();
-        // shared_ptr<FatropApplication> solver = ocpbuilder.Build();
-        // usage of parameter setter
-        // ocpbuilder.GetParameterSetter("target_pos")->SetValue({1., 1., 1.});
-        // cout << "Calling the solver " << endl;
-        app-> Optimize();
-        // usage of parameter sampler
-        // auto res = app-> GetSampler("Frame") -> Sample(app-> LastSolution(), app->GlobalParameters(), app->StageParameters());
-    // }
-    // else
-    // {
-    //     cout << "run me as RunFatrop f.so f.json" << endl;
-    // }
+        app -> Optimize();
+    }
+    else
+    {
+        cout << "run me as RunFatrop f.so f.json" << endl;
+    }
 }
