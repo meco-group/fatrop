74cc39effed751d37fb2e192a6bca5fdf6c5ada4
landerv <lander.vanroye@kuleuven.be>
2023-03-14 18:18:09 +0100

    options wip

--- fatrop/solver/FatropParams.hpp
+++ fatrop/solver/FatropParams.hpp
@@ -7,58 +7,102 @@
 using namespace std;
 namespace fatrop
 {
-    struct NumericOption
+    template<typename T>
+    struct Option
     {
     public:
         // NumericOption operator=(const NumericOption &other) = default;
-        NumericOption(){};
-        NumericOption(const string &name, const string &description, double *value, double default_value, bool lower_bound_inclusive, double lower_bound, bool upper_bound_inclusive, double upper_bound):
-            name_(name), description_(description), value(value), default_value_(default_value), lower_bound_inclusive_(lower_bound_inclusive), lower_bound_(lower_bound), upper_bound_inclusive_(upper_bound_inclusive), upper_bound_(upper_bound)
+        Option(){};
+        Option(const string &name, const string &description, T *value, T default_value, bool lower_bound_inclusive, T lower_bound, bool upper_bound_inclusive, T upper_bound) : name_(name), description_(description), value(value), default_value_(default_value), lower_bound_inclusive_(lower_bound_inclusive), lower_bound_(lower_bound), upper_bound_inclusive_(upper_bound_inclusive), upper_bound_(upper_bound){};
+        static Option<T> LowerBounded(const string &name, const string &description, T *value, T default_value, T lower_bound)
         {
+            return Option<T>(name, description, value, default_value, true, lower_bound, false, 0.0);
         };
-        static NumericOption LowerBounded(const string &name, const string &description, double *value, double default_value, double lower_bound)
+        static Option<T> UpperBounded(const string &name, const string &description, T *value, T default_value, T upper_bound)
         {
-            return NumericOption(name, description, value, default_value, true, lower_bound, false, 0.0);
+            return Option<T>(name, description, value, default_value, false, 0.0, true, upper_bound);
         };
-        static NumericOption UpperBounded(const string &name, const string &description, double *value, double default_value, double upper_bound)
+        static Option<T> UnBounded(const string &name, const string &description, T *value, T default_value)
         {
-            return NumericOption(name, description, value, default_value, false, 0.0, true, upper_bound);
+            return Option<T>(name, description, value, default_value, false, 0.0, false, 0.0);
         };
-        static NumericOption UnBounded(const string &name, const string &description, double *value, double default_value)
+        static Option<T> BoxBounded(const string &name, const string &description, T *value, T default_value, T lower_bound, T upper_bound)
         {
-            return NumericOption(name, description, value, default_value, false, 0.0, false, 0.0);
-        };
-        static NumericOption BoxBounded(const string &name, const string &description, double *value, double default_value, double lower_bound, double upper_bound)
-        {
-            return NumericOption(name, description, value, default_value, true, lower_bound, true, upper_bound);
+            return Option<T>(name, description, value, default_value, true, lower_bound, true, upper_bound);
         };
         string name_;
         string description_;
-        double *value = NULL;
-        double default_value_;
+        T *value = NULL;
+        T default_value_;
         bool lower_bound_inclusive_;
-        double lower_bound_;
+        T lower_bound_;
         bool upper_bound_inclusive_;
-        double upper_bound_;
+        T upper_bound_;
     };
+    template <>
+    struct Option<string> 
+    {
+        string name_;
+    };
+    template <>
+    struct Option<bool> 
+    {
+        string name_;
+    };
+    // define Numeric option as Option<double>
+    typedef Option<double> NumericOption;
+    typedef Option<int> IntegerOption;
+    typedef Option<string> StringOption;
+    typedef Option<bool> BooleanOption;
 
     class FatropOptions
     {
     public:
         FatropOptions()
         {
+            // register integer options
+            // RegisterIntegerOption(IntegerOption::LowerBounded("max_iter", "maximum number of iterations", &max_iter, 1000, 0));
+            RegisterIntegerOption(IntegerOption::LowerBounded("max_watchdog_steps", "maximum number of watchdog steps", &max_watchdog_steps, 4, 0));
+            // RegisterIntegerOption(IntegerOption::LowerBounded("max_watchdog_steps", "maximum number of watchdog steps", &max_watchdog_steps, 4, 0));
             // register tolerance option
             RegisterNumericOption(NumericOption::LowerBounded("tol", "tolerance", &tol, 1e-8, 0.0));
             RegisterNumericOption(NumericOption::LowerBounded("acceptable_tol", "acceptable tolerance", &acceptable_tol, 1e-6, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("smax", "smax", &smax, 100.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("lammax", "lammax", &lammax, 1e3, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("mu_init", "mu_init", &mu0, 1e2, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_eta", "kappa_eta", &kappa_eta, 10.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_mu", "kappa_mu", &kappa_mu, 0.2, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("theta_mu", "theta_mu", &theta_mu, 1.5, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("delta_w0", "delta_w0", &delta_w0, 1e-4, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("delta_wmin", "delta_wmin", &delta_wmin, 1e-20, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_wmin", "kappa_wmin", &kappa_wmin, 1.0 / 3.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_wplus", "kappa_wplus", &kappa_wplus, 8.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_wplusem", "kappa_wplusem", &kappa_wplusem, 100.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_sigma", "kappa_sigma", &kappa_sigma, 1e10, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("s_phi", "s_phi", &s_phi, 2.3, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("delta", "delta", &delta, 1.0, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("s_theta", "s_theta", &s_theta, 1.1, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("theta_min", "theta_min", &theta_min, 1e-4, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("gamma_theta", "gamma_theta", &gamma_theta, 1e-12, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("gamma_phi", "gamma_phi", &gamma_phi, 1e-8, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("gamma_alpha", "gamma_alpha", &gamma_alpha, 0.05, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("eta_phi", "eta_phi", &eta_phi, 1e-8, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("delta_c_stripe", "delta_c_stripe", &delta_c_stripe, 1e-2, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_c", "kappa_c", &kappa_c, 0.25, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa1", "kappa1", &kappa1, 1e-2, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa2", "kappa2", &kappa2, 1e-2, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("kappa_d", "kappa_d", &kappa_d, 1e-5, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("bound_relax_factor", "bound_relax_factor", &bound_relax_factor, 1e-8, 0.0));
+            RegisterNumericOption(NumericOption::LowerBounded("constr_viol_tol", "constr_viol_tol", &constr_viol_tol, 1e-4, 0.0));
         };
 
         int max_watchdog_steps = 4;
         bool warm_start_dual = false;
         int maxiter = 1000; // TODO unsafe to change maxiter because it it used at building!!
-        double smax = 100.0;
-        double lammax = 1e3;
         double tol = 1e-8;
         double acceptable_tol = 1e-6;
+        double smax = 100.0;
+        double lammax = 1e3;
         double acceptable_iter = 15;
         double mu0 = 1e2;
         // double mu0 = 2e5;
@@ -90,7 +134,7 @@ namespace fatrop
         double kappa_d = 1e-5;
         double bound_relax_factor = 1e-8;
         double constr_viol_tol = 1e-4; // currently only used to relax bounds
-        void SetNumericOption(const string& option_name, double value)
+        void SetNumericOption(const string &option_name, double value)
         {
             // check if option exists
             if (numeric_options.find(option_name) == numeric_options.end())
@@ -98,7 +142,7 @@ namespace fatrop
                 throw runtime_error("Option " + option_name + " does not exist.");
             }
             // check if value is in bounds
-            NumericOption& option = numeric_options[option_name];
+            NumericOption &option = numeric_options[option_name];
             if (option.lower_bound_inclusive_ && value < option.lower_bound_)
             {
                 throw runtime_error("Option " + option_name + " must be greater than or equal to " + to_string(option.lower_bound_));
@@ -110,12 +154,48 @@ namespace fatrop
             // set value
             *option.value = value;
         }
+        void SetIntegerOption(const string &option_name, int value)
+        {
+            // check if option exists
+            if (integer_options.find(option_name) == integer_options.end())
+            {
+                throw runtime_error("Option " + option_name + " does not exist.");
+            }
+            // check if value is in bounds
+            IntegerOption &option = integer_options[option_name];
+            if (option.lower_bound_inclusive_ && value < option.lower_bound_)
+            {
+                throw runtime_error("Option " + option_name + " must be greater than or equal to " + to_string(option.lower_bound_));
+            }
+            if (option.upper_bound_inclusive_ && value > option.upper_bound_)
+            {
+                throw runtime_error("Option " + option_name + " must be less than or equal to " + to_string(option.upper_bound_));
+            }
+            // set value
+            *option.value = value;
+        }
+
     private:
         void RegisterNumericOption(const NumericOption &option)
         {
             numeric_options[option.name_] = option;
         }
+        void RegisterIntegerOption(const IntegerOption &option)
+        {
+            integer_options[option.name_] = option;
+        }
+        void RegisterStringOption(const StringOption &option)
+        {
+            string_options[option.name_] = option;
+        }
+        void RegisterBooleanOption(const BooleanOption &option)
+        {
+            boolean_options[option.name_] = option;
+        }
         map<string, NumericOption> numeric_options;
+        map<string, IntegerOption> integer_options;
+        map<string, StringOption> string_options;
+        map<string, BooleanOption> boolean_options;
     };
 
 } // namespace fatrop
