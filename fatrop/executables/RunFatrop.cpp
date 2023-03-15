b193638147b94a80b6f6a806c1a6fa6101835da5
landerv <lander.vanroye@kuleuven.be>
2023-03-15 11:16:46 +0100

    update readme, runfatrop

--- fatrop/executables/RunFatrop.cpp
+++ fatrop/executables/RunFatrop.cpp
@@ -5,33 +5,12 @@
 using namespace fatrop;
 int main(int argc, char **argv)
 {
-
-    class CustoMExpression : public StageExpression
-    {
-        void Eval(const double *u, const double *x, const double *global_params, const double *stage_params, double *res)
-        {
-            res[0] = u[0];
-            res[1] = u[0] * cos(x[0]);
-        };
-        int n_rows()
-        {
-            return 2;
-        }
-
-        int n_cols()
-        {
-            return 1;
-        }
-    };
-
     if (argc == 3)
     {
         shared_ptr<StageOCPApplication> app = StageOCPApplicationBuilder::FromRockitInterface(argv[1], argv[2]);
-        auto custom_evaluator = app->GetExprEvaluator(make_shared<CustoMExpression>())->at_control();
         app->SetNumericOption("tol", 1e-6);
         app->Optimize();
         vector<double> result = (app->LastStageOCPSolution()).Eval(app->GetExprEvaluator("control_u")->at_t0());
-        vector<double> result_custom = (app->LastStageOCPSolution()).Eval(custom_evaluator);
     }
     else
     {
