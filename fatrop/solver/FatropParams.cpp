af65e4a11b9fcd3a587460a837a2f3366aff44ab
landerv <lander.vanroye@kuleuven.be>
2023-03-16 21:40:25 +0100

    cleanup

--- fatrop/solver/FatropParams.cpp
+++ fatrop/solver/FatropParams.cpp
@@ -1,2 +1,142 @@
 #include "solver/FatropParams.hpp"
 using namespace fatrop;
+
+Option<bool>::Option(const string &name, const string &description, bool *value, bool default_value) : name_(name), description_(description), value(value), default_value_(default_value){};
+void Option<bool>::Set(const bool &new_value)
+{
+    *value = new_value;
+}
+void Option<bool>::SetDefault() const
+{
+    *value = default_value_;
+}
+
+template <typename T>
+Option<T>::Option(const string &name, const string &description, T *value, T default_value, bool lower_bound_inclusive, T lower_bound, bool upper_bound_inclusive, T upper_bound) : name_(name), description_(description), value(value), default_value_(default_value), lower_bound_inclusive_(lower_bound_inclusive), lower_bound_(lower_bound), upper_bound_inclusive_(upper_bound_inclusive), upper_bound_(upper_bound){};
+template <typename T>
+Option<T> Option<T>::LowerBounded(const string &name, const string &description, T *value, T default_value, T lower_bound)
+{
+    return Option<T>(name, description, value, default_value, true, lower_bound, false, 0.0);
+};
+template <typename T>
+Option<T> Option<T>::UpperBounded(const string &name, const string &description, T *value, T default_value, T upper_bound)
+{
+    return Option<T>(name, description, value, default_value, false, 0.0, true, upper_bound);
+};
+template <typename T>
+Option<T> Option<T>::UnBounded(const string &name, const string &description, T *value, T default_value)
+{
+    return Option<T>(name, description, value, default_value, false, 0.0, false, 0.0);
+};
+template <typename T>
+Option<T> Option<T>::BoxBounded(const string &name, const string &description, T *value, T default_value, T lower_bound, T upper_bound)
+{
+    return Option<T>(name, description, value, default_value, true, lower_bound, true, upper_bound);
+};
+template <typename T>
+void Option<T>::SetDefault() const
+{
+    *value = default_value_;
+};
+template <typename T>
+void Option<T>::Set(const T &new_value)
+{
+    // check if new value is in bounds
+    if (lower_bound_inclusive_ && new_value < lower_bound_)
+    {
+        throw runtime_error("Option " + name_ + " is out of bounds");
+    }
+    if (upper_bound_inclusive_ && new_value > upper_bound_)
+    {
+        throw runtime_error("Option " + name_ + " is out of bounds");
+    }
+    *value = new_value;
+};
+FatropOptions::FatropOptions()
+{
+    RegisterOption(IntegerOption::BoxBounded("max_iter", "maximum number of iterations", &maxiter, 1000, 0, maxiter));
+    RegisterOption(NumericOption::LowerBounded("kappa_d", "kappa_d", &kappa_d, 1e-5, 0.0));
+};
+template <typename T>
+void FatropOptions::SetOption(const string &option_name, T value)
+{
+    if (numeric_options.find(option_name) != numeric_options.end())
+    {
+        if constexpr (std::is_floating_point<T>::value)
+        {
+            numeric_options[option_name].Set(value);
+        }
+        else
+        {
+            throw std::runtime_error("Option " + option_name + " not of type double");
+        }
+    }
+    else if (integer_options.find(option_name) != integer_options.end())
+    {
+        if constexpr (std::is_integral<T>::value)
+        {
+            integer_options[option_name].Set(value);
+        }
+        else
+        {
+            throw std::runtime_error("Option " + option_name + " not of type int");
+        }
+    }
+    else if (boolean_options.find(option_name) != boolean_options.end())
+    {
+        if constexpr (std::is_same<T, bool>::value)
+        {
+            boolean_options[option_name].Set(value);
+        }
+    }
+    else
+    {
+        throw std::runtime_error("Option " + option_name + " not found");
+    }
+}
+
+void FatropOptions::RegisterOption(const NumericOption &option)
+{
+    numeric_options[option.name_] = option;
+    option.SetDefault();
+}
+void FatropOptions::RegisterOption(const IntegerOption &option)
+{
+    integer_options[option.name_] = option;
+    option.SetDefault();
+}
+void FatropOptions::RegisterOption(const BooleanOption &option)
+{
+    boolean_options[option.name_] = option;
+    option.SetDefault();
+}
+auto operator<<(std::ostream &os, const FatropOptions &m) -> std::ostream &
+{
+    os << "Numeric options :" << std::endl;
+    {
+        for (auto const &x : m.numeric_options)
+        {
+            os << "   " << x.first << " : " << *x.second.value << std::endl;
+        }
+    }
+    os << "Integer options :" << std::endl;
+    {
+        for (auto const &x : m.integer_options)
+        {
+            os << "   " << x.first << " : " << *x.second.value << std::endl;
+        }
+    }
+    os << "Boolean options :" << std::endl;
+    {
+        for (auto const &x : m.boolean_options)
+        {
+            os << "   " << x.first << " : " << *x.second.value << std::endl;
+        }
+    }
+    return os;
+}
+template class Option<int>;
+template class Option<double>;
+template void FatropOptions::SetOption<double>(const string&, double);
+template void FatropOptions::SetOption<int>(const string&, int);
+template void FatropOptions::SetOption<bool>(const string&, bool);
--- fatrop/solver/FatropParams.hpp
+++ fatrop/solver/FatropParams.hpp
@@ -15,40 +15,13 @@ namespace fatrop
     public:
         // NumericOption operator=(const NumericOption &other) = default;
         Option(){};
-        Option(const string &name, const string &description, T *value, T default_value, bool lower_bound_inclusive, T lower_bound, bool upper_bound_inclusive, T upper_bound) : name_(name), description_(description), value(value), default_value_(default_value), lower_bound_inclusive_(lower_bound_inclusive), lower_bound_(lower_bound), upper_bound_inclusive_(upper_bound_inclusive), upper_bound_(upper_bound){};
-        static Option<T> LowerBounded(const string &name, const string &description, T *value, T default_value, T lower_bound)
-        {
-            return Option<T>(name, description, value, default_value, true, lower_bound, false, 0.0);
-        };
-        static Option<T> UpperBounded(const string &name, const string &description, T *value, T default_value, T upper_bound)
-        {
-            return Option<T>(name, description, value, default_value, false, 0.0, true, upper_bound);
-        };
-        static Option<T> UnBounded(const string &name, const string &description, T *value, T default_value)
-        {
-            return Option<T>(name, description, value, default_value, false, 0.0, false, 0.0);
-        };
-        static Option<T> BoxBounded(const string &name, const string &description, T *value, T default_value, T lower_bound, T upper_bound)
-        {
-            return Option<T>(name, description, value, default_value, true, lower_bound, true, upper_bound);
-        };
-        void SetDefault() const
-        {
-            *value = default_value_;
-        };
-        void Set(const T &new_value)
-        {
-            // check if new value is in bounds
-            if (lower_bound_inclusive_ && new_value < lower_bound_)
-            {
-                throw runtime_error("Option " + name_ + " is out of bounds");
-            }
-            if (upper_bound_inclusive_ && new_value > upper_bound_)
-            {
-                throw runtime_error("Option " + name_ + " is out of bounds");
-            }
-            *value = new_value;
-        };
+        Option(const string &name, const string &description, T *value, T default_value, bool lower_bound_inclusive, T lower_bound, bool upper_bound_inclusive, T upper_bound); 
+        static Option<T> LowerBounded(const string &name, const string &description, T *value, T default_value, T lower_bound);
+        static Option<T> UpperBounded(const string &name, const string &description, T *value, T default_value, T upper_bound);
+        static Option<T> UnBounded(const string &name, const string &description, T *value, T default_value);
+        static Option<T> BoxBounded(const string &name, const string &description, T *value, T default_value, T lower_bound, T upper_bound);
+        void SetDefault() const;
+        void Set(const T &new_value);
         string name_;
         string description_;
         T *value = NULL;
@@ -58,35 +31,20 @@ namespace fatrop
         bool upper_bound_inclusive_;
         T upper_bound_;
     };
-    // template <>
-    // struct Option<string>
-    // {
-    //     Option<string>(const string& name, const string& descrription, )
-    //     string name_;
-    //     // void Set(const string& new_value)
-    //     // {
-    //     //     *value = new_value;
-    //     // }
-    // };
     template <>
     struct Option<bool>
     {
-        Option<bool>(){};
-        Option<bool>(const string &name, const string &description, bool *value, bool default_value) : name_(name), description_(description), value(value), default_value_(default_value){};
-        void Set(const bool &new_value)
-        {
-            *value = new_value;
-        }
-        void SetDefault() const
-        {
-            *value = default_value_;
-        }
+        Option(){};
+        Option(const string &name, const string &description, bool *value, bool default_value);
+        void Set(const bool &new_value);
+        void SetDefault() const;
         string name_;
         string description_;
         bool *value = NULL;
         bool default_value_;
     };
-    // define Numeric option as Option<double>
+
+   // define Numeric option as Option<double>
     typedef Option<double> NumericOption;
     typedef Option<int> IntegerOption;
     typedef Option<string> StringOption;
@@ -95,107 +53,17 @@ namespace fatrop
     class FatropOptions
     {
     public:
-        FatropOptions()
-        {
-            RegisterOption(IntegerOption::BoxBounded("max_iter", "maximum number of iterations", &maxiter, 1000, 0, maxiter));
-            RegisterOption(NumericOption::LowerBounded("kappa_d", "kappa_d", &kappa_d, 1e-5, 0.0));
-        };
+        FatropOptions();
         int maxiter = 1000; // TODO this value cannot be changed to a value larger than the one used for building the solver
         double kappa_d = 1e-5;
         template <typename T>
-        void SetOption(const string &option_name, T value)
-        {
-            if (numeric_options.find(option_name) != numeric_options.end())
-            {
-                if constexpr (std::is_floating_point<T>::value)
-                {
-                    numeric_options[option_name].Set(value);
-                }
-                else
-                {
-                    throw std::runtime_error("Option " + option_name + " not of type double");
-                }
-            }
-            else if (integer_options.find(option_name) != integer_options.end())
-            {
-                if constexpr (std::is_integral<T>::value)
-                {
-                    integer_options[option_name].Set(value);
-                }
-                else
-                {
-                    throw std::runtime_error("Option " + option_name + " not of type int");
-                }
-            }
-            // else if (string_options.find(option_name) != string_options.end())
-            // {
-            //     if const_expr(std::is_same<T, string>::value())
-            //     {
-            //         string_options[option_name].Set(value);
-            //     }
-            //     else
-            //     {
-            //         throw std::runtime_error("Option " + option_name + " not of type string");
-            //     }
-            // }
-            else if (boolean_options.find(option_name) != boolean_options.end())
-            {
-                if constexpr (std::is_same<T, bool>::value)
-                {
-                    boolean_options[option_name].Set(value);
-                }
-            }
-            else
-            {
-                throw std::runtime_error("Option " + option_name + " not found");
-            }
-        }
+        void SetOption(const string &option_name, T value);
 
     public:
-        void RegisterOption(const NumericOption &option)
-        {
-            numeric_options[option.name_] = option;
-            option.SetDefault();
-        }
-        void RegisterOption(const IntegerOption &option)
-        {
-            integer_options[option.name_] = option;
-            option.SetDefault();
-        }
-        // void RegisterStringOption(const StringOption &option)
-        // {
-        //     string_options[option.name_] = option;
-        // }
-        void RegisterOption(const BooleanOption &option)
-        {
-            boolean_options[option.name_] = option;
-            option.SetDefault();
-        }
-        friend auto operator<<(std::ostream &os, const FatropOptions &m) -> std::ostream &
-        {
-            os << "Numeric options :" << std::endl;
-            {
-                for (auto const &x : m.numeric_options)
-                {
-                    os << "   " << x.first << " : " << *x.second.value << std::endl;
-                }
-            }
-            os << "Integer options :" << std::endl;
-            {
-                for (auto const &x : m.integer_options)
-                {
-                    os << "   " << x.first << " : " << *x.second.value << std::endl;
-                }
-            }
-            os << "Boolean options :" << std::endl;
-            {
-                for (auto const &x : m.boolean_options)
-                {
-                    os << "   " << x.first << " : " << *x.second.value << std::endl;
-                }
-            }
-            return os;
-        }
+        void RegisterOption(const NumericOption &option);
+        void RegisterOption(const IntegerOption &option);
+        void RegisterOption(const BooleanOption &option);
+        friend auto operator<<(std::ostream &os, const FatropOptions &m) -> std::ostream &;
         map<string, NumericOption> numeric_options;
         map<string, IntegerOption> integer_options;
         // map<string, StringOption> string_options;
