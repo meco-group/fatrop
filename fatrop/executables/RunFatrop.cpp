c22d4172a55cfafc17a7ed452124b9ba5be16978
landerv <lander.vanroye@kuleuven.be>
2023-03-16 14:04:56 +0100

    more usage details in comments

--- fatrop/executables/RunFatrop.cpp
+++ fatrop/executables/RunFatrop.cpp
@@ -7,13 +7,24 @@ int main(int argc, char **argv)
 {
     if (argc == 3)
     {
+        //// dynamic memory allocation  
         shared_ptr<StageOCPApplication> app = StageOCPApplicationBuilder::FromRockitInterface(argv[1], argv[2]);
-        app->SetOption("tol", 1e-6);
+        // auto eval_expression = app->GetExprEvaluator("control_u")->at_t0();
+        // vector<double> u0_result(eval_expression -> Size());
+        ///  no dynamic memory allocation
         app->Optimize();
-        vector<double> result = (app->LastStageOCPSolution()).Eval(app->GetExprEvaluator("control_u")->at_t0());
-        app->SetInitial(app->LastStageOCPSolution());
-        app -> SetOption("warm_start_init_point", true);
-        app -> Optimize();
+        // ///  retrieve solution
+        // app->LastStageOCPSolution().Eval(eval_expression, u0_result);
+        // ///  initialize next solver run 
+        // app->SetInitial(app->LastStageOCPSolution());
+        // ///  change solver options
+        // app->SetOption("tol", 1e-6);
+        // app->SetOption("mu_init", 1e-5);
+        // app->SetOption("bound_push", 1e-7); // all *_bound_push variables
+        // app->SetOption("accept_every_trial_step", false);
+        // app->SetOption("iterative_refinement", false); // fast_step_computation
+        // app->SetOption("warm_start_init_point", true);
+        // app->Optimize();
     }
     else
     {
