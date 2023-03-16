8bdda000289861f40a5d8d576dddfa151e0bd5a1
landerv <lander.vanroye@kuleuven.be>
2023-03-16 15:34:08 +0100

    extra options

--- fatrop/executables/RunFatrop.cpp
+++ fatrop/executables/RunFatrop.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
     {
         //// dynamic memory allocation  
         shared_ptr<StageOCPApplication> app = StageOCPApplicationBuilder::FromRockitInterface(argv[1], argv[2]);
-        // auto eval_expression = app->GetExprEvaluator("control_u")->at_t0();
-        // vector<double> u0_result(eval_expression -> Size());
+        auto eval_expression = app->GetExprEvaluator("control_u")->at_t0();
+        vector<double> u0_result(eval_expression -> Size());
         ///  no dynamic memory allocation
         app->Optimize();
         // ///  retrieve solution
@@ -21,10 +21,11 @@ int main(int argc, char **argv)
         // app->SetOption("tol", 1e-6);
         // app->SetOption("mu_init", 1e-5);
         // app->SetOption("bound_push", 1e-7); // all *_bound_push variables
+        // app->SetOption("warm_start_mult_bound_push", 1e-7); 
         // app->SetOption("accept_every_trial_step", false);
         // app->SetOption("iterative_refinement", false); // fast_step_computation
         // app->SetOption("warm_start_init_point", true);
-        // app->Optimize();
+        // // app->Optimize();
     }
     else
     {
