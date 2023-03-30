75c9399bd8c04e90e8abe6703af75d25f46d2c01
landerv <lander.vanroye@kuleuven.be>
2023-03-30 18:04:06 +0200

    copyright header

--- fatrop/ocp/StageOCPExpressions.hpp
+++ fatrop/ocp/StageOCPExpressions.hpp
@@ -1,3 +1,21 @@
+/*
+ * Fatrop - A fast trajectory optimization solver
+ * Copyright (C) 2022, 2023 Lander Vanroye <lander.vanroye@kuleuven.be>
+ *
+ * This file is part of Fatrop.
+ *
+ * Fatrop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fatrop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fatrop.  If not, see <http://www.gnu.org/licenses/>. */
 #ifndef BASICOCPSAMPLERSINCLUDED
 #define BASICOCPSAMPLERSINCLUDED
 #include "function_evaluation/FunctionEvaluation.hpp"
