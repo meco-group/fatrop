6efd69553673f327fc25fd996629b910f272e5eb
landerv <lander.vanroye@kuleuven.be>
2023-06-28 10:27:45 +0200

    modified license header

--- fatrop/ocp/OCPDims.hpp
+++ fatrop/ocp/OCPDims.hpp
@@ -1,13 +1,21 @@
-/**
- * @file FatropOCP.hpp
- * @author your name (you@domain.com)
- * @brief this file contains necessary the OCP specific code
- * @version 0.1
- * @date 2021-11-10
+/*
+ * Fatrop - A fast trajectory optimization solver
+ * Copyright (C) 2022, 2023 Lander Vanroye, KU Leuven. All rights reserved.
  *
- * @copyright Copyright (c) 2021
+ * This file is part of Fatrop.
  *
- */
+ * Fatrop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fatrop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fatrop.  If not, see <http://www.gnu.org/licenses/>. */
 #ifndef FATROP_OCPDIMS_INCLUDED
 #define FATROP_OCPDIMS_INCLUDED
 #include "auxiliary/FatropVector.hpp"
