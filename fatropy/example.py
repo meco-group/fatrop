399e6e246720c77b09c05665034b5929fbf0fa7e
landerv <lander.vanroye@kuleuven.be>
2023-08-22 14:54:31 +0200

    update example.py

--- fatropy/example.py
+++ fatropy/example.py
@@ -1,8 +1,10 @@
 import fatropy
 import numpy as np
-ocp = fatropy.StageOCPApplicationFactory.from_rockit_interface("/home/lander/projects/fatrop_rockit_demo/foobar/casadi_codegen.so", "/home/lander/projects/fatrop_rockit_demo/foobar/casadi_codegen.json")
+rockit_generated_so = "foobar/casadi_codegen.so"
+rockit_generated_json = "foobar/casadi_codegen.json"
+ocp = fatropy.StageOCPApplicationFactory.from_rockit_interface(rockit_generated_so, rockit_generated_json)
 ocp.optimize()
-sol = ocp.last_stageocp_solution()
+sol = ocp.last_solution()
 # ocp.set_initial_x(sol.x)
 # ocp.set_initial_u(sol.u)
 ocp.set_initial(sol)
@@ -10,5 +12,5 @@ ocp.set_option("mu_init", 1e-3)
 ocp.set_value("target", [1., 2])
 # ocp.set_option("bound_push", 1e-6)
 ocp.optimize()
-print(ocp.last_stageocp_solution().x)
+print(ocp.last_solution().x)
 # print(sol.evaluate(ocp.get_expression("control_u").at_control()))
