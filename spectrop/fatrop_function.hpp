09dfbfe868028a9320afc5fbc2a04725ac4f40e7
landerv <lander.vanroye@kuleuven.be>
2023-10-25 11:39:25 +0200

    fatrop function with functioninternal

--- spectrop/fatrop_function.hpp
+++ spectrop/fatrop_function.hpp
@@ -9,79 +9,46 @@ namespace fatrop
     namespace spectrop
     {
         namespace cs = casadi;
-        class FatropFunctionInternal : public cs::CallbackInternal
+        class FatropFunctionInternal : public cs::FunctionInternal
         {
         public:
-            FatropFunctionInternal(const std::string &name, cs::Callback *cb, const std::shared_ptr<fatrop::OCPApplication> &app_) : cs::CallbackInternal(name, cb), app(app_)
+            FatropFunctionInternal(const std::string &name, const std::shared_ptr<fatrop::OCPApplication> &app_) : cs::FunctionInternal(name), app(app_)
             {
                 // cs::CallbackInternal::construct(name, cb);
+                fatrop::OCPDims dims = app->get_ocp_dims();
+                n_vars = dims.n_u_tot + dims.n_x_tot;
+                n_stage_params = dims.n_stage_params_tot;
+                n_global_params = dims.n_global_params;
+                arg_initial_vars = std::vector<double>(n_vars);
+                arg_stage_parameters = std::vector<double>(n_stage_params);
+                arg_global_parameters = std::vector<double>(n_global_params);
             }
-            std::vector<double> arg_initial_vars;
-            std::vector<double> arg_stage_parameters;
-            std::vector<double> arg_global_parameters;
-            std::shared_ptr<fatrop::OCPApplication> app;
-            int n_vars;
-            int n_stage_params;
-            int n_global_params;
-        };
-        class FatropFunction : public cs::Callback
-        {
-        public:
-            FatropFunction(const std::shared_ptr<fatrop::OCPAbstract> &ocpimpl_)
-            {
-                auto app_ = std::make_shared<fatrop::OCPApplication>(ocpimpl_);
-                app_->build();
-                // cs::Callback::construct("FatropFunction");
-                if (!is_null())
-                {
-                    casadi_error("Cannot create 'FatropFunction': Internal class already created");
-                }
-                auto ptr = new FatropFunctionInternal("FatropFunction", this, app_);
-                static_cast<cs::SharedObject *>(this)->own(ptr);
-                auto options_ = cs::Dict();
-                auto internal_p = static_cast<FatropFunctionInternal *>(this->get());
-                fatrop::OCPDims dims = internal_p->app->get_ocp_dims();
-                internal_p->n_vars = dims.n_u_tot + dims.n_x_tot;
-                internal_p->n_stage_params = dims.n_stage_params_tot;
-                internal_p->n_global_params = dims.n_global_params;
-                internal_p->arg_initial_vars = std::vector<double>(internal_p->n_vars);
-                internal_p->arg_stage_parameters = std::vector<double>(internal_p->n_stage_params);
-                internal_p->arg_global_parameters = std::vector<double>(internal_p->n_global_params);
-                static_cast<cs::Function *>(this)->operator->()->casadi::FunctionInternal::construct(options_);
-            }
-            ~FatropFunction() override
-            {
-                // bypass the destructor of cs::Callback
-                // this -> cs::Function::~Function();
-            };
-            casadi_int get_n_in() override
+            size_t get_n_in() override
             {
                 return 3; // initial vars, stage_parameters, global_paramets
             }
-            casadi_int get_n_out() override
+            size_t get_n_out() override
             {
                 return 1;
             }
             cs::Sparsity get_sparsity_in(casadi_int i) override
             {
-                auto internal_p = static_cast<FatropFunctionInternal *>(this->get());
                 if (i == 0)
                 {
-                    return cs::Sparsity::dense(internal_p->n_vars, 1);
+                    return cs::Sparsity::dense(n_vars, 1);
                 }
                 else if (i == 1)
                 {
-                    return cs::Sparsity::dense(internal_p->n_stage_params, 1);
+                    return cs::Sparsity::dense(n_stage_params, 1);
                 }
                 // else
                 // {
-                return cs::Sparsity::dense(internal_p->n_global_params, 1);
+                return cs::Sparsity::dense(n_global_params, 1);
                 // }
             }
             cs::Sparsity get_sparsity_out(casadi_int i) override
             {
-                auto internal_p = static_cast<FatropFunctionInternal *>(this->get());
-                return cs::Sparsity::dense(internal_p->n_vars, 1);
+                return cs::Sparsity::dense(n_vars, 1);
             }
 
             std::string get_name_in(casadi_int i) override
@@ -105,38 +72,54 @@ namespace fatrop
                 return "vars";
             }
 
-            std::vector<cs::DM> eval(const std::vector<cs::DM> &args) const override
-            {
-                auto internal_p = static_cast<FatropFunctionInternal *>(this->get());
-                // inefficient implementation with dynamic memory allocations
-                std::vector<double> initial_vars = args[0].nonzeros();
-                std::vector<double> stage_parameters = args[1].nonzeros();
-                std::vector<double> global_parameters = args[2].nonzeros();
-                internal_p->app->set_initial(initial_vars);
-                internal_p->app->set_params(global_parameters, stage_parameters);
-                internal_p->app->optimize();
-                auto retv = std::vector<double>(internal_p->n_vars);
-                internal_p->app->last_solution_primal().copyto(retv);
-                auto ret = std::vector<cs::DM>{retv};
-                return ret;
-            }
-            int eval_buffer(const double **arg, const std::vector<casadi_int> &sizes_arg,
-                            double **res, const std::vector<casadi_int> &sizes_res) const override
+            int eval(const double **arg,
+                     double **res, casadi_int *iw, double *w, void *mem) const override
             {
                 // no dynamic memory allocations here
-                auto internal_p = static_cast<FatropFunctionInternal *>(this->get());
-                std::copy(arg[0], arg[0] + internal_p->n_vars, (double *)internal_p->arg_initial_vars.data());
-                std::copy(arg[1], arg[1] + internal_p->n_stage_params, (double *)internal_p->arg_stage_parameters.data());
-                std::copy(arg[2], arg[2] + internal_p->n_global_params, (double *)internal_p->arg_global_parameters.data());
-                internal_p->app->set_initial(internal_p->arg_initial_vars);
-                internal_p->app->set_params(internal_p->arg_global_parameters, internal_p->arg_stage_parameters);
-                internal_p->app->optimize();
-                double *last_sol = ((VEC *)internal_p->app->last_solution_primal())->pa;
+                std::copy(arg[0], arg[0] + n_vars, (double *)arg_initial_vars.data());
+                std::copy(arg[1], arg[1] + n_stage_params, (double *)arg_stage_parameters.data());
+                std::copy(arg[2], arg[2] + n_global_params, (double *)arg_global_parameters.data());
+                app->set_initial(arg_initial_vars);
+                app->set_params(arg_global_parameters, arg_stage_parameters);
+                app->optimize();
+                double *last_sol = ((VEC *)app->last_solution_primal())->pa;
                 if (res[0])
-                    std::copy(last_sol, last_sol + internal_p->n_vars, res[0]);
+                    std::copy(last_sol, last_sol + n_vars, res[0]);
                 return 0;
             }
-            bool has_eval_buffer() const override { return true; };
+            // destructor
+            ~FatropFunctionInternal() override
+            {
+                clear_mem();
+            }
+
+            std::string class_name() const override { return "FatropFunction"; }
+            std::vector<double> arg_initial_vars;
+            std::vector<double> arg_stage_parameters;
+            std::vector<double> arg_global_parameters;
+            std::shared_ptr<fatrop::OCPApplication> app;
+            int n_vars;
+            int n_stage_params;
+            int n_global_params;
+        };
+
+        class FatropFunction : public cs::Function
+        {
+        public:
+            FatropFunction(const std::shared_ptr<fatrop::OCPAbstract> &ocpimpl_)
+            {
+                auto app_ = std::make_shared<fatrop::OCPApplication>(ocpimpl_);
+                app_->build();
+                // cs::Callback::construct("FatropFunction");
+                if (!is_null())
+                {
+                    casadi_error("Cannot create 'FatropFunction': Internal class already created");
+                }
+                auto ptr = new FatropFunctionInternal("FatropFunction", app_);
+                static_cast<cs::SharedObject *>(this)->own(ptr);
+                auto options_ = cs::Dict();
+                static_cast<cs::Function *>(this)->operator->()->casadi::FunctionInternal::construct(options_);
+            }
 
         private:
         };
