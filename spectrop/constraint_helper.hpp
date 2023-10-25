e9800583a0d081d91c30912a296ad74c0cbc387f
landerv <lander.vanroye@kuleuven.be>
2023-10-25 16:05:20 +0200

    cleanup

--- spectrop/constraint_helper.hpp
+++ spectrop/constraint_helper.hpp
@@ -19,10 +19,10 @@ namespace fatrop
                                 casadi::MX &g)
             {
                 // reset lb, ub, g_ineq, g
-                lb = casadi::DM();
-                ub = casadi::DM();
-                g_ineq = casadi::MX();
-                g = casadi::MX();
+                std::vector<cs::DM> lb_vec;
+                std::vector<cs::DM> ub_vec;
+                std::vector<cs::MX> g_ineq_vec;
+                std::vector<cs::MX> g_vec;
                 assert(constr.size2() == 1);
                 auto opti = casadi::Opti();
                 auto syms = casadi::MX::symvar(constr);
@@ -39,15 +39,19 @@ namespace fatrop
                 {
                     if ((double)lb_temp(i) == (double)ub_temp(i))
                     {
-                        g = casadi::MX::veccat({g, g_temp(i) - lb_temp(i)});
+                        g_vec.push_back(g_temp(i) - lb_temp(i));
                     }
                     else
                     {
-                        g_ineq = casadi::MX::veccat({g_ineq, g_temp(i)});
-                        lb = casadi::DM::veccat({lb, lb_temp(i)});
-                        ub = casadi::DM::veccat({ub, ub_temp(i)});
+                        g_ineq_vec.push_back(g_temp(i));
+                        lb_vec.push_back(lb_temp(i));
+                        ub_vec.push_back(ub_temp(i));
                     }
                 }
+                lb = cs::DM::veccat(lb_vec);
+                ub = cs::DM::veccat(ub_vec);
+                g_ineq = cs::MX::veccat(g_ineq_vec);
+                g = cs::MX::veccat(g_vec);
             }
             static void process(const std::vector<casadi::MX> &constr,
                                 casadi::DM &lb,
@@ -55,10 +59,10 @@ namespace fatrop
                                 casadi::MX &g_ineq,
                                 casadi::MX &g)
             {
-                lb = casadi::DM();
-                ub = casadi::DM();
-                g_ineq = casadi::MX();
-                g = casadi::MX();
+                std::vector<cs::DM> lb_vec;
+                std::vector<cs::DM> ub_vec;
+                std::vector<cs::MX> g_ineq_vec;
+                std::vector<cs::MX> g_vec;
                 for (auto &constr_i : constr)
                 {
                     auto lb_i = casadi::DM();
@@ -66,11 +70,15 @@ namespace fatrop
                     auto g_ineq_i = casadi::MX();
                     auto g_i = casadi::MX();
                     process(constr_i, lb_i, ub_i, g_ineq_i, g_i);
-                    lb = casadi::DM::veccat({lb, lb_i});
-                    ub = casadi::DM::veccat({ub, ub_i});
-                    g_ineq = casadi::MX::veccat({g_ineq, g_ineq_i});
-                    g = casadi::MX::veccat({g, g_i});
+                    lb_vec.push_back(lb_i);
+                    ub_vec.push_back(ub_i);
+                    g_ineq_vec.push_back(g_ineq_i);
+                    g_vec.push_back(g_i);
                 }
+                lb = cs::DM::veccat(lb_vec);
+                ub = cs::DM::veccat(ub_vec);
+                g_ineq = cs::MX::veccat(g_ineq_vec);
+                g = cs::MX::veccat(g_vec);
             }
         };
     }
