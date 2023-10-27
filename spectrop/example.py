f295681e40f72ed5602331acf8b9100c6c2ff926
landerv <lander.vanroye@kuleuven.be>
2023-10-27 15:30:58 +0200

    cleanup

--- spectrop/example.py
+++ spectrop/example.py
@@ -12,7 +12,7 @@ x_next = x + vertcat(x[1], e * x[1] - x[0] + u) * dt
 # /*
 #   =----  initial stage ----=
 # */
-initial_stage = ocp.new_stage() # // states and controls are derived automatically
+initial_stage = ocp.new_stage() # states and controls are derived automatically
 # constraint 
 initial_stage.subject_to(x[0] == 1.)
 # constraint 
@@ -25,7 +25,7 @@ initial_stage.add_objective(u * u + sumsqr(x))
 # 
 #   =----  middle stage ----=
 # 
-middle_stage = ocp.new_stage(19) #; // 18 is the number of nodes, states, controls and parameters are derived automatically
+middle_stage = ocp.new_stage(19) # 19 is the number of nodes, states, controls and parameters are derived automatically
 # constraints 
 middle_stage.subject_to(-0.25 < x[1])
 middle_stage.subject_to((-1.0 < u) < 1)
@@ -37,7 +37,7 @@ middle_stage.add_objective(u * u + sumsqr(x))
 # 
 #   =----  terminal stage ----=
 # 
-terminal_stage = ocp.new_stage() #; // the last stage also has x2 as state
+terminal_stage = ocp.new_stage() # the last stage also has x2 as state
 # constraints  
 terminal_stage.subject_to(-0.25 < x[1])
 terminal_stage.subject_to(x[1] == p)
