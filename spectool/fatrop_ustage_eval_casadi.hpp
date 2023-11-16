e74111f8c3595e4fee25bdac6dd536773d0eb471
landerv <lander.vanroye@kuleuven.be>
2023-11-16 15:57:18 +0100

    prepare for more customization

--- spectool/fatrop_ustage_eval_casadi.hpp
+++ spectool/fatrop_ustage_eval_casadi.hpp
@@ -31,53 +31,99 @@ namespace fatrop
                 cs::MX lam_g_equality = cs::MX::sym("lam_g_equality", ng_eq_);
                 cs::MX lam_g_inequality = cs::MX::sym("lam_g_inequality", ng_ineq_);
                 cs::MX lam_dynamics = cs::MX::sym("lam_dynamics", nxp1_);
+                auto xp1 = cs::MX::sym("xp1", nxp1_);
                 std::vector<cs::MX> ustage_syms{sq.u, sq.x, sq.p_stage, sq.p_global};
-                auto ux = cs::MX::veccat({sq.u, sq.x});
+
+
+                auto hess_lag_obj = hess_lag_obj_sym(sq);
+                auto dynamics_jac = dynamics_jacobian_sym(sq, xp1);
+                auto equality_jac = equality_jacobian_sym(sq);
+                auto inequality_jac = inequality_jacobian_sym(sq);
+                auto hess_lag = hess_lag_sym(sq, lam_dynamics, lam_g_equality, lam_g_inequality);
+
+                auto BAbt = cs::MX::horzcat({dynamics_jac.first, dynamics_jac.second}).T();
+                auto Ggt_equality = cs::MX::horzcat({equality_jac.first, equality_jac.second}).T();
+                auto Ggt_inequality = cs::MX::horzcat({inequality_jac.first, inequality_jac.second}).T();
+                auto RSQrqt = cs::MX::horzcat({hess_lag.first, hess_lag.second}).T();
+
+
                 L_ = CasadiFEWrap(cs::Function("L", ustage_syms, {sq.L}, optss), expand, jit, jit_options_, eval_cache);
-                auto rq_sym = cs::MX();
-                auto RSQ_sym = cs::MX::hessian(sq.L, ux, rq_sym);
-                rq_ = CasadiFEWrap(cs::Function("rq", ustage_syms, {cs::MX::densify(rq_sym)}, optss), expand, jit, jit_options_, eval_cache);
-                // if (sq.nxp1 > 0)
-                {
-                    auto xp1 = cs::MX::sym("xp1", nxp1_);
-                    auto b = -xp1 + sq.x_next;
-                    auto BAbt = cs::MX::horzcat({cs::MX::jacobian(sq.x_next, ux), b}).T();
-                    b_ = CasadiFEWrap(cs::Function("b", {sq.x, xp1, sq.u, sq.p_stage, sq.p_global},
-                                                   {densify(b)}, optss),
-                                      expand, jit, jit_options_, eval_cache);
-                    BAbt_ = CasadiFEWrap(cs::Function("BAbt", {sq.x, xp1, sq.u, sq.p_stage, sq.p_global},
-                                                      {cs::MX::densify(BAbt)}, optss),
-                                         expand, jit, jit_options_, eval_cache);
-                    auto rq_dyn_sym = cs::MX();
-                    auto RSQ_dyn_sym = cs::MX::hessian(cs::MX::dot(sq.x_next, lam_dynamics), ux, rq_dyn_sym);
-                    rq_sym += rq_dyn_sym;
-                    RSQ_sym += RSQ_dyn_sym;
-                }
-                // if(sq.ng_eq > 0)
-                {
-                    auto g_eq = sq.g;
-                    auto Ggt_equality = cs::MX::horzcat({cs::MX::jacobian(g_eq, ux), g_eq}).T();
-                    Ggt_equality_ = CasadiFEWrap(cs::Function("Ggt_equality", ustage_syms, {cs::MX::densify(Ggt_equality)}, optss), expand, jit, jit_options_, eval_cache);
-                    g_equality_ = CasadiFEWrap(cs::Function("g_equality", ustage_syms, {cs::MX::densify(g_eq)}, optss), expand, jit, jit_options_, eval_cache);
-                    auto rq_eq = cs::MX();
-                    auto RSQ_eq = cs::MX::hessian(cs::MX::dot(g_eq, lam_g_equality), ux, rq_eq);
-                    rq_sym += rq_eq;
-                    RSQ_sym += RSQ_eq;
-                }
-                // if(sq.ng_ineq>0)
-                {
-                    auto g_ineq = sq.g_ineq;
-                    auto Ggt_inequality = cs::MX::horzcat({cs::MX::jacobian(g_ineq, ux), g_ineq}).T();
-                    Ggt_inequality_ = CasadiFEWrap(cs::Function("Ggt_inequality", ustage_syms, {cs::MX::densify(Ggt_inequality)}, optss), expand, jit, jit_options_, eval_cache);
-                    g_inequality_ = CasadiFEWrap(cs::Function("g_inequality", ustage_syms, {cs::MX::densify(g_ineq)}, optss), expand, jit, jit_options_, eval_cache);
-                    auto rq_ineq = cs::MX();
-                    auto RSQ_ineq = cs::MX::hessian(cs::MX::dot(g_ineq, lam_g_inequality), ux, rq_ineq);
-                    rq_sym += rq_ineq;
-                    RSQ_sym += RSQ_ineq;
-                }
-                RSQrqt_ = CasadiFEWrap(cs::Function("RSQrqt", {sq.u, sq.x, lam_dynamics, lam_g_equality, lam_g_inequality, sq.p_stage, sq.p_global}, {cs::MX::densify(cs::MX::horzcat({RSQ_sym, rq_sym}).T())}, optss), expand, jit, jit_options_, eval_cache);
+                rq_ = CasadiFEWrap(cs::Function("rq", ustage_syms, {cs::MX::densify(hess_lag_obj.second)}, optss), expand, jit, jit_options_, eval_cache);
+                b_ = CasadiFEWrap(cs::Function("b", {sq.x, xp1, sq.u, sq.p_stage, sq.p_global},
+                                                {densify(dynamics_jac.second)}, optss),
+                                    expand, jit, jit_options_, eval_cache);
+                BAbt_ = CasadiFEWrap(cs::Function("BAbt", {sq.x, xp1, sq.u, sq.p_stage, sq.p_global},
+                                                    {cs::MX::densify(BAbt)}, optss),
+                                        expand, jit, jit_options_, eval_cache);
+                Ggt_equality_ = CasadiFEWrap(cs::Function("Ggt_equality", ustage_syms, {cs::MX::densify(Ggt_equality)}, optss), expand, jit, jit_options_, eval_cache);
+                g_equality_ = CasadiFEWrap(cs::Function("g_equality", ustage_syms, {cs::MX::densify(equality_jac.second)}, optss), expand, jit, jit_options_, eval_cache);
+                Ggt_inequality_ = CasadiFEWrap(cs::Function("Ggt_inequality", ustage_syms, {cs::MX::densify(Ggt_inequality)}, optss), expand, jit, jit_options_, eval_cache);
+                g_inequality_ = CasadiFEWrap(cs::Function("g_inequality", ustage_syms, {cs::MX::densify(inequality_jac.second)}, optss), expand, jit, jit_options_, eval_cache);
+                RSQrqt_ = CasadiFEWrap(cs::Function("RSQrqt", {sq.u, sq.x, lam_dynamics, lam_g_equality, lam_g_inequality, sq.p_stage, sq.p_global}, {cs::MX::densify(RSQrqt)}, optss), expand, jit, jit_options_, eval_cache);
                 Ub_ = sq.ub.nonzeros();
                 Lb_ = sq.lb.nonzeros();
+            }
+            virtual std::pair<cs::MX, cs::MX> dynamics_jacobian_sym(const uStageQuantities& sq, const cs::MX& xp1)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto G = cs::MX::jacobian(sq.x_next, ux);
+                auto g = -xp1 + sq.x_next;
+                return std::make_pair(G, g);
+            }
+
+            virtual std::pair<cs::MX, cs::MX> equality_jacobian_sym(const uStageQuantities& sq)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto G = cs::MX::jacobian(sq.g, ux);
+                auto g = sq.g;
+                return std::make_pair(G, g);
+            }
+
+            virtual std::pair<cs::MX, cs::MX> inequality_jacobian_sym(const uStageQuantities& sq)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto G = cs::MX::jacobian(sq.g_ineq, ux);
+                auto g = sq.g_ineq;
+                return std::make_pair(G, g);
+            }
+
+            virtual std::pair<cs::MX, cs::MX> hess_lag_obj_sym(const uStageQuantities& sq)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto rq_sym = cs::MX();
+                auto RSQ_sym = cs::MX::hessian(sq.L, ux, rq_sym);
+                return std::make_pair(RSQ_sym, rq_sym);
+            }
+            virtual std::pair<cs::MX, cs::MX> hess_lag_dyn_sym(const uStageQuantities& sq, const cs::MX& lam_dyn)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto rq_sym = cs::MX();
+                auto RSQ_sym = cs::MX::hessian(cs::MX::dot(sq.x_next, lam_dyn), ux, rq_sym);
+                return std::make_pair(RSQ_sym, rq_sym);
+            }
+            virtual std::pair<cs::MX, cs::MX> hess_lag_eq_sym(const uStageQuantities& sq, const cs::MX& lam_g_equality)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto rq_sym = cs::MX();
+                auto RSQ_sym = cs::MX::hessian(cs::MX::dot(sq.g, lam_g_equality), ux, rq_sym);
+                return std::make_pair(RSQ_sym, rq_sym);
+            }
+            virtual std::pair<cs::MX, cs::MX> hess_lag_ineq_sym(const uStageQuantities& sq, const cs::MX& lam_g_inequality)
+            {
+                auto ux = cs::MX::veccat({sq.u, sq.x});
+                auto rq_sym = cs::MX();
+                auto RSQ_sym = cs::MX::hessian(cs::MX::dot(sq.g_ineq, lam_g_inequality), ux, rq_sym);
+                return std::make_pair(RSQ_sym, rq_sym);
+            }
+            virtual std::pair<cs::MX, cs::MX> hess_lag_sym(const uStageQuantities&sq, const cs::MX& lam_dyn, const cs::MX& lam_g_equality, const cs::MX& lam_g_inequality)
+            {
+                auto hess_obj = hess_lag_obj_sym(sq);
+                auto hess_dyn = hess_lag_dyn_sym(sq, lam_dyn);
+                auto hess_eq = hess_lag_eq_sym(sq, lam_g_equality);
+                auto hess_ineq = hess_lag_ineq_sym(sq, lam_g_inequality);
+                auto RSQ_sym = hess_obj.first + hess_dyn.first + hess_eq.first + hess_ineq.first;
+                auto rq_sym = hess_obj.second + hess_dyn.second + hess_eq.second + hess_ineq.second;
+                return std::make_pair(RSQ_sym, rq_sym);
             }
 
         public:
