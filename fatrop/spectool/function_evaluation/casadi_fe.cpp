4aeba96c0f6f307609a11b1117febfb129668c44
landerv <lander.vanroye@kuleuven.be>
2024-01-24 15:27:52 +0100

    support for multithreaded FE in spectool (omp)

--- fatrop/spectool/function_evaluation/casadi_fe.cpp
+++ fatrop/spectool/function_evaluation/casadi_fe.cpp
@@ -16,12 +16,26 @@ namespace fatrop
             sz_arg = func.n_in();
             sz_res = func.n_out();
             func.sz_work(sz_arg, sz_res, sz_iw, sz_w);
+#ifndef ENABLE_MULTITHREADING
             iw.resize(sz_iw);
             w.resize(sz_w);
             bufout.resize(func.nnz_out(0));
             bufdata.resize(sz_res > 0 ? sz_res : 1);
             resdata.resize(sz_res > 0 ? sz_res : 1);
             argdata.resize(sz_arg > 0 ? sz_arg : 1);
+#else
+            auto num_threads = omp_get_max_threads();
+            for (int thread_i = 0; thread_i < num_threads; thread_i++)
+            {
+                iw[thread_i].resize(sz_iw);
+                w[thread_i].resize(sz_w);
+                bufout[thread_i].resize(func.nnz_out(0));
+                bufdata[thread_i].resize(sz_res > 0 ? sz_res : 1);
+                resdata[thread_i].resize(sz_res > 0 ? sz_res : 1);
+                argdata[thread_i].resize(sz_arg > 0 ? sz_arg : 1);
+            }
+#endif
+
             n_in = func.n_in();
             if (jit)
             {
@@ -44,13 +58,20 @@ namespace fatrop
         };
         void CasadiFEWrap::eval(const double **args, MAT *res)
         {
-            // assert(res->m == m);
-            // assert(res->n == n);
+// assert(res->m == m);
+// assert(res->n == n);
+#ifndef ENABLE_MULTITHREADING
             eval(args, bufout.data());
             PACKMAT(m, n, bufout.data(), m, res, 0, 0);
+#else
+            int tid = omp_get_thread_num();
+            eval(args, bufout[tid].data());
+            PACKMAT(m, n, bufout[tid].data(), m, res, 0, 0);
+#endif
         };
         void CasadiFEWrap::eval(const double **args, double *res)
         {
+#ifndef ENABLE_MULTITHREADING
             // inputs
             for (int j = 0; j < n_in; j++)
                 argdata[j] = args[j];
@@ -60,6 +81,18 @@ namespace fatrop
                 resdata[0] = res;
                 jit_ ? fe_jit_->eval_(argdata.data(), resdata.data(), iw.data(), w.data(), 0) : func_(argdata.data(), resdata.data(), iw.data(), w.data(), 0);
             }
+#else
+            int tid = omp_get_thread_num();
+            // inputs
+            for (int j = 0; j < n_in; j++)
+                argdata[tid][j] = args[j];
+            // outputs
+            if (res)
+            {
+                resdata[tid][0] = res;
+                jit_ ? fe_jit_->eval_(argdata[tid].data(), resdata[tid].data(), iw[tid].data(), w[tid].data(), 0) : func_(argdata[tid].data(), resdata[tid].data(), iw[tid].data(), w[tid].data(), 0);
+            }
+#endif
         };
 
     }
--- fatrop/spectool/function_evaluation/casadi_fe.hpp
+++ fatrop/spectool/function_evaluation/casadi_fe.hpp
@@ -5,6 +5,9 @@
 #include "fatrop/blasfeo_wrapper/LinearAlgebraBlasfeo.hpp"
 #include "casadi_jit.hpp"
 #include "casadi_jit_cache.hpp"
+#ifdef ENABLE_MULTITHREADING
+#include <omp.h>
+#endif
 namespace fatrop
 {
     namespace spectool
@@ -14,7 +17,7 @@ namespace fatrop
         {
         public:
             CasadiFEWrap(){};
-            CasadiFEWrap(const cs::Function &func, bool expand, bool jit, const cs::Dict& jit_options_, CasadiJitCache& cache_map);
+            CasadiFEWrap(const cs::Function &func, bool expand, bool jit, const cs::Dict &jit_options_, CasadiJitCache &cache_map);
             void eval(const double **args, MAT *res);
             void eval(const double **args, double *res);
 
@@ -25,12 +28,21 @@ namespace fatrop
             int mem;
             int n_in;
             bool jit_;
+#ifndef ENABLE_MULTITHREADING
             std::vector<double> bufout;
             std::vector<double *> bufdata;
             std::vector<double *> resdata;
             std::vector<const double *> argdata;
             std::vector<long long int> iw;
             std::vector<double> w;
+#else
+            std::vector<std::vector<double>>  bufout = std::vector<std::vector<double>>(omp_get_max_threads()) ;
+            std::vector<std::vector<double *>>  bufdata = std::vector<std::vector<double *>>(omp_get_max_threads()) ;
+            std::vector<std::vector<double *>>  resdata = std::vector<std::vector<double *>>(omp_get_max_threads()) ;
+            std::vector<std::vector<const double *>>  argdata = std::vector<std::vector<const double *>>(omp_get_max_threads()) ;
+            std::vector<std::vector<long long int>>  iw = std::vector<std::vector<long long int>>(omp_get_max_threads()) ;
+            std::vector<std::vector<double>>  w = std::vector<std::vector<double>>(omp_get_max_threads()) ;
+#endif
             std::shared_ptr<CasadiFEJit> fe_jit_;
         };
         // implementation of OCPAbstract, given an OCP
