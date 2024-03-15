897a0e168c3ad171c2a5d0c53903f6b1e66c907f
landerv <lander.vanroye@kuleuven.be>
2024-03-15 16:32:30 +0100

    example update params

--- fatrop/spectool/examples/walker2d_multi_steps.py
+++ fatrop/spectool/examples/walker2d_multi_steps.py
@@ -4,8 +4,8 @@ import numpy as np
 import fatropy.spectool as sp
 import robot2d as r2d
 
-N = 50
-N_steps = 5
+N = 10
+N_steps = 4
 T = .5
 ocp = sp.Ocp()
 
@@ -22,8 +22,8 @@ all_steps = []
 all_mechanisms = []
 
 for step in range(N_steps):
-    ddq_left, mechanism_left, links_left = walker2d_dynamics.ode_left_contact(q, qd, tau, 2*step*0.25)
-    ddq_right, mechanism_right, links_right = walker2d_dynamics.ode_right_contact(q, qd, tau, (2*step+1)*0.25)
+    ddq_left, mechanism_left, links_left = walker2d_dynamics.ode_left_contact(q, qd, tau, 2*step*0.25-1.)
+    ddq_right, mechanism_right, links_right = walker2d_dynamics.ode_right_contact(q, qd, tau, (2*step+1)*0.25-1.)
     all_mechanisms += (mechanism_left, mechanism_right)
     stage_left = ocp.new_stage(N)
     stage_right = ocp.new_stage(N)
@@ -45,17 +45,17 @@ for step in range(N_steps):
 
     # transition constraints
     foot_right = links_left[-1]
-    stage_left.at_tf().subject_to(foot_right.left.origin() == [(2*step+1)*0.25, 0.0])
+    stage_left.at_tf().subject_to(foot_right.left.origin() == [(2*step+1)*0.25-1., 0.0])
     stage_left.at_tf().subject_to(cs.sin(foot_right.left.theta) == 0.) # theta = 0. + k * pi
     if step != N_steps - 1:
         foot_left = links_right[-1]
-        stage_right.at_tf().subject_to(foot_left.left.origin() == [(2*step+2)*0.25, 0.0])
+        stage_right.at_tf().subject_to(foot_left.left.origin() == [(2*step+2)*0.25-1., 0.0])
         stage_right.at_tf().subject_to(cs.sin(foot_left.left.theta) == 0.) # theta = 0. + k * pi
 
     # Define the cost function
     for stage in [stage_left, stage_right]:
         stage.add_objective(cs.sumsqr(qd), sp.t0, sp.mid, sp.tf)
-        stage.add_objective(1e-1*cs.sumsqr(tau), sp.t0, sp.mid)
+        stage.add_objective(cs.sumsqr(tau), sp.t0, sp.mid)
     # no over-bending of knees
     for stage in [stage_left, stage_right]:
         stage.subject_to(0. <= q[2], sp.mid)
@@ -69,10 +69,11 @@ ocp.at_tf().subject_to(q == qf)
 ocp.at_tf().subject_to(qd == qdf)
 
 
-ocp.solver('fatrop', {"post_expand":True, "jit":False}, {"tol":1e-4, "mu_init":1e4})
+ocp.solver('fatrop', {"post_expand":True, "jit":False}, {})
 
 fun = ocp.to_function("fun", [], [ocp.sample(q)[1], stage_left.at_tf(foot_right.left.origin())])
 
+sol = fun()
 sol = fun()
 q_sol = sol['o0']
 r2d.animate([(all_mechanisms[i], q, q_sol[:, i*(N+1):(i+1)*(N+1)-1]) for i in range(2*N_steps)])
