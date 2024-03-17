135ed699cffdc81195e59a3baecd6a91536c4331
landerv <lander.vanroye@kuleuven.be>
2024-03-17 14:06:14 +0100

    add model with arms

--- fatrop/spectool/examples/walker2d_dynamics.py
+++ fatrop/spectool/examples/walker2d_dynamics.py
@@ -43,7 +43,6 @@ def ode_left_contact(q, dq, tau, x_foot_left):
     ddq, _, _ = mechanism.get_dynamics(W, q, dq, tau)
     return ddq, mechanism, links
 
-
 def ode_right_contact(q, dq, tau, x_foot_right):
     theta_foot_left, dtheta_foot_left = q[0], dq[0]
     theta_foot_right, dtheta_foot_right = q[1], dq[1]
@@ -66,6 +65,90 @@ def ode_right_contact(q, dq, tau, x_foot_right):
     foot_left_joint = r2d.revolute(lower_leg_left.right.transform(r2d.transform2d(0, 0, np.pi/2)), -theta_foot_left, -dtheta_foot_left)
     foot_left = r2d.link(foot_left_joint, m_foot, l_foot)
     links = [lower_leg_left, upper_leg_left, body, upper_leg_right, lower_leg_right, foot_left]
+    # W = F_left * lower_leg_left.left.x + M_left*lower_leg_left.left.theta
+    W = tau.T@q
+    # built up the mechanism
+    mechanism = r2d.mechanism(links)
+    ddq, _, _ = mechanism.get_dynamics(W, q, dq, tau)
+    return ddq, mechanism, links
+
+
+def ode_left_contact_arms(q, dq, tau, x_foot_left):
+    theta_foot_left, dtheta_foot_left = q[0], dq[0]
+    theta_foot_right, dtheta_foot_right = q[1], dq[1]
+    theta_knee_left, dtheta_knee_left = q[2], dq[2]
+    theta_knee_right, dtheta_knee_right = q[3], dq[3]
+    theta_hip_left, dtheta_hip_left = q[4], dq[4]
+    theta_hip_right, dtheta_hip_right = q[5], dq[5]
+    shoulder_left, dshoulder_left = q[6], dq[6]
+    shoulder_right, dshoulder_right = q[7], dq[7]
+    elbow_left, delbow_left = q[8], dq[8]
+    elbow_right, delbow_right = q[9], dq[9]
+    # built up the mechanism
+    foot_contact_p = r2d.transform2d(x_foot_left, 0, 0)
+    foot_left_joint = r2d.revolute(foot_contact_p.transform(r2d.transform2d(0, 0, np.pi/2)), theta_foot_left, dtheta_foot_left)
+    lower_leg_left = r2d.link(foot_left_joint, m_leg_upper, l_lleg_upper)
+    knee_left_joint = r2d.revolute(lower_leg_left.right, theta_knee_left, dtheta_knee_left)
+    upper_leg_left = r2d.link(knee_left_joint, m_leg_upper, l_lleg_upper)
+    hip_left_joint = r2d.revolute(upper_leg_left.right, theta_hip_left, dtheta_hip_left)
+    body = r2d.link(hip_left_joint, m_body, l_body)
+    hip_right_joint = r2d.revolute(body.left.transform(r2d.transform2d(0, 0, np.pi)), theta_hip_right, dtheta_hip_right)
+    upper_leg_right = r2d.link(hip_right_joint, m_leg_upper, l_lleg_upper)
+    knee_right_joint = r2d.revolute(upper_leg_right.right, theta_knee_right, dtheta_knee_right)
+    lower_leg_right = r2d.link(knee_right_joint, m_leg_upper, l_lleg_upper)
+    foot_right_joint = r2d.revolute(lower_leg_right.right.transform(r2d.transform2d(0, 0, np.pi/2)), theta_foot_right, dtheta_foot_right)
+    foot_right = r2d.link(foot_right_joint, m_foot, l_foot)
+    arm_left_joint = r2d.revolute(body.right.transform(r2d.transform2d(0, 0, np.pi)), shoulder_left, dshoulder_left)
+    arm_left = r2d.link(arm_left_joint, m_leg_upper, l_lleg_upper)
+    arm_right_joint = r2d.revolute(body.right.transform(r2d.transform2d(0, 0, np.pi)), shoulder_right, dshoulder_right)
+    arm_right = r2d.link(arm_right_joint, m_leg_upper, l_lleg_upper)
+    arm2_left_joint = r2d.revolute(arm_left.right, elbow_left, delbow_left)
+    arm2_left = r2d.link(arm2_left_joint, m_leg_upper, l_lleg_upper)
+    arm2_right_joint = r2d.revolute(arm_right.right, elbow_right, delbow_right)
+    arm2_right = r2d.link(arm2_right_joint, m_leg_upper, l_lleg_upper)
+    # collect all the links
+    links = [arm_left, arm_right, arm2_left, arm2_right, lower_leg_left, upper_leg_left, body, upper_leg_right, lower_leg_right, foot_right]
+    # W = F_left * lower_leg_left.left.x + M_left*lower_leg_left.left.theta
+    W = tau.T@q
+    # built up the mechanism
+    mechanism = r2d.mechanism(links)
+    ddq, _, _ = mechanism.get_dynamics(W, q, dq, tau)
+    return ddq, mechanism, links
+
+def ode_right_contact_arms(q, dq, tau, x_foot_right):
+    theta_foot_left, dtheta_foot_left = q[0], dq[0]
+    theta_foot_right, dtheta_foot_right = q[1], dq[1]
+    theta_knee_left, dtheta_knee_left = q[2], dq[2]
+    theta_knee_right, dtheta_knee_right = q[3], dq[3]
+    theta_hip_left, dtheta_hip_left = q[4], dq[4]
+    theta_hip_right, dtheta_hip_right = q[5], dq[5]
+    shoulder_left, dshoulder_left = q[6], dq[6]
+    shoulder_right, dshoulder_right = q[7], dq[7]
+    elbow_left, delbow_left = q[8], dq[8]
+    elbow_right, delbow_right = q[9], dq[9]
+    # built up the mechanism
+    foot_contact_p = r2d.transform2d(x_foot_right, 0, 0.)
+    foot_right_joint = r2d.revolute(foot_contact_p.transform(r2d.transform2d(0, 0, np.pi/2)), -theta_foot_right, -dtheta_foot_right)
+    lower_leg_right = r2d.link(foot_right_joint, m_leg_upper, l_lleg_upper)
+    knee_right_joint = r2d.revolute(lower_leg_right.right, -theta_knee_right, -dtheta_knee_right)
+    upper_leg_right = r2d.link(knee_right_joint, m_leg_upper, l_lleg_upper)
+    hip_right_joint = r2d.revolute(upper_leg_right.right, -theta_hip_right, -dtheta_hip_right)
+    body = r2d.link(hip_right_joint, m_body, l_body)
+    hip_left_joint = r2d.revolute(body.left.transform(r2d.transform2d(0, 0, -np.pi)), -theta_hip_left, -dtheta_hip_left)
+    upper_leg_left = r2d.link(hip_left_joint, m_leg_upper, l_lleg_upper)
+    knee_left_joint = r2d.revolute(upper_leg_left.right, -theta_knee_left, -dtheta_knee_left)
+    lower_leg_left = r2d.link(knee_left_joint, m_leg_upper, l_lleg_upper)
+    foot_left_joint = r2d.revolute(lower_leg_left.right.transform(r2d.transform2d(0, 0, np.pi/2)), -theta_foot_left, -dtheta_foot_left)
+    foot_left = r2d.link(foot_left_joint, m_foot, l_foot)
+    arm_left_joint = r2d.revolute(body.right.transform(r2d.transform2d(0, 0, np.pi)), shoulder_left, dshoulder_left)
+    arm_left = r2d.link(arm_left_joint, m_leg_upper, l_lleg_upper)
+    arm_right_joint = r2d.revolute(body.right.transform(r2d.transform2d(0, 0, np.pi)), shoulder_right, dshoulder_right)
+    arm_right = r2d.link(arm_right_joint, m_leg_upper, l_lleg_upper)
+    arm2_left_joint = r2d.revolute(arm_left.right, elbow_left, delbow_left)
+    arm2_left = r2d.link(arm2_left_joint, m_leg_upper, l_lleg_upper)
+    arm2_right_joint = r2d.revolute(arm_right.right, elbow_right, delbow_right)
+    arm2_right = r2d.link(arm2_right_joint, m_leg_upper, l_lleg_upper)
+    links = [arm_left, arm_right, arm2_left, arm2_right, lower_leg_left, upper_leg_left, body, upper_leg_right, lower_leg_right, foot_left]
     # W = F_left * lower_leg_left.left.x + M_left*lower_leg_left.left.theta
     W = tau.T@q
     # built up the mechanism
---
+++ fatrop/spectool/examples/walker2d_multi_steps_arms.py
@@ -0,0 +1,84 @@
+import walker2d_dynamics
+import casadi as cs
+import numpy as np
+import fatropy.spectool as sp
+import robot2d as r2d
+
+N = 10
+N_steps = 4
+T = .5
+ocp = sp.Ocp()
+
+q = ocp.state(10)
+qd = ocp.state(10)
+tau = ocp.control(10)
+
+q0 = cs.DM([0, 0, 0, 0, 0 , 0., 0., 0., 0., 0.]) # foot_left, foot_right, knee_left, knee_right, hip_left, hip_right
+qd0 = cs.DM([0, 0, 0, 0, 0, 0., 0., 0., 0., 0.])
+qf = cs.DM([0, 0., 0, 0, 0, 0., 0., 0., 0., 0.])
+qdf = cs.DM([0, 0, 0, 0, 0, 0., 0., 0., 0., 0.])
+
+all_steps = []
+all_mechanisms = []
+
+for step in range(N_steps):
+    ddq_left, mechanism_left, links_left = walker2d_dynamics.ode_left_contact_arms(q, qd, tau, 2*step*0.25-1.)
+    ddq_right, mechanism_right, links_right = walker2d_dynamics.ode_right_contact_arms(q, qd, tau, (2*step+1)*0.25-1.)
+    all_mechanisms += (mechanism_left, mechanism_right)
+    stage_left = ocp.new_stage(N)
+    stage_right = ocp.new_stage(N)
+    all_steps.append((stage_left, stage_right))
+    # Define the dynamics
+
+    stage_left.set_next(q, q + qd * T / N)
+    stage_left.set_next(qd, qd + ddq_left * T / N)
+
+    stage_right.set_next(q, q + qd * T / N)
+    stage_right.set_next(qd, qd + ddq_right * T / N)
+
+    # transition dynamics
+    stage_left.at_tf().set_next(q, q)
+    stage_left.at_tf().set_next(qd, qd)
+    if step != N_steps - 1:
+        stage_right.at_tf().set_next(q, q)
+        stage_right.at_tf().set_next(qd, qd)
+
+    # transition constraints
+    foot_right = links_left[-1]
+    stage_left.at_tf().subject_to(foot_right.left.origin() == [(2*step+1)*0.25-1., 0.0])
+    stage_left.at_tf().subject_to(cs.sin(foot_right.left.theta/2) == 0.) # theta = 0. + k * pi
+    if step != N_steps - 1:
+        foot_left = links_right[-1]
+        stage_right.at_tf().subject_to(foot_left.left.origin() == [(2*step+2)*0.25-1., 0.0])
+        stage_right.at_tf().subject_to(cs.sin(foot_left.left.theta/2) == 0.) # theta = 0. + k * pi
+
+    # Define the cost function
+    for stage in [stage_left, stage_right]:
+        stage.add_objective(cs.sumsqr(tau), sp.t0, sp.mid, sp.tf)
+        stage.add_objective(1e-1*cs.sumsqr(qd), sp.t0, sp.mid)
+
+    # no over-bending of knee
+    for stage in [stage_left, stage_right]:
+        stage.subject_to(0. <= q[2], sp.mid)
+        stage.subject_to(0. >= q[3], sp.mid)
+    # no torque in wrong direction from feet
+    stage_left.subject_to(tau[0]<0., sp.mid)
+    stage_right.subject_to(tau[1]>0., sp.mid)
+
+
+# Define the constraints
+ocp.at_t0().subject_to(q == q0)
+ocp.at_t0().subject_to(qd == qd0)
+ocp.at_tf().subject_to(q == qf)
+ocp.at_tf().subject_to(qd == qdf)
+
+ocp.set_initial(q, np.array([0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0.]))
+
+
+ocp.solver('fatrop', {"post_expand":True, "jit":False}, {})
+
+fun = ocp.to_function("fun", [], [ocp.sample(q)[1], stage_left.at_tf(foot_right.left.origin())])
+
+sol = fun()
+q_sol = sol['o0']
+r2d.animate([(all_mechanisms[i], q, q_sol[:, i*(N+1):(i+1)*(N+1)-1]) for i in range(2*N_steps)])
