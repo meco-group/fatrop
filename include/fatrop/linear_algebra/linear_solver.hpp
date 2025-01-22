976d39c0a691b99a7bf6460848c749fec9258d3c
Lander Vanroye <lander.vanroye@kuleuven.be>
2025-01-22 15:47:33 +0100

    linsol: seperate declr and def

--- include/fatrop/linear_algebra/linear_solver.hpp
+++ include/fatrop/linear_algebra/linear_solver.hpp
@@ -4,83 +4,29 @@
 
 #ifndef __fatrop_linear_algebra_linear_solver_hpp__
 #define __fatrop_linear_algebra_linear_solver_hpp__
+
 #include "fatrop/context/context.hpp"
 #include "fatrop/linear_algebra/fwd.hpp"
-#include "fatrop/linear_algebra/linear_algebra.hpp"
+#include "fatrop/linear_algebra/vector.hpp"
 #include "fatrop/linear_algebra/linear_solver_return_flags.hpp"
-#include "fatrop/linear_algebra/linear_system.hpp"
 #include <limits> // for std::numeric_limits
 
 namespace fatrop
 {
-    template <typename Derived, typename LsType> class LinearSolver
+    template <typename Derived, typename LsType>
+    class LinearSolver
     {
     public:
         LinearSolver(const Index m) : m_(m), x(m), x_(m_), residual_(m_), tmp_(m_) {}
         Derived &derived() { return *static_cast<Derived *>(this); }
+
         // solve without iterative refinement
-        LinsolReturnFlag solve_once(LinearSystem<LsType> &ls, VecRealView &x_in)
-        {
-            return derived().solve_once_impl(ls, x_in);
-        }
-        void solve_rhs(LinearSystem<LsType> &ls, VecRealView &x_in)
-        {
-            return derived().solve_rhs_impl(ls, x_in);
-        }
+        LinsolReturnFlag solve_once(LinearSystem<LsType> &ls, VecRealView &x_in);
+
+        void solve_rhs(LinearSystem<LsType> &ls, VecRealView &x_in);
+
         // solve with iterative refinement
-        LinsolReturnFlag solve_in_place(LinearSystem<LsType> &ls)
-        {
-            // reset x
-            vecse(m_, 0.0, x_, 0);
-            // solve
-            LinsolReturnFlag ret = solve_once(ls, x_);
-            if (ret != LinsolReturnFlag::SUCCESS)
-            {
-                return ret;
-            }
-            // reset residual
-            vecse(m_, 0.0, residual_, 0);
-            // "accept" x_
-            veccp(m_, x_, 0, x, 0);
-            Scalar res_prev = std::numeric_limits<Scalar>::max();
-            ls.get_rhs(tmp_);
-            const Scalar b_norm = norm_inf(tmp_);
-            for (Index i = 0; i < max_it_ref; i++)
-            {
-                // compute residual residual = Ax + b
-                ls.apply_on_right(x_, 1.0, tmp_, residual_);
-                // compute residual inf norm
-                const Scalar res_norm = norm_inf(residual_) / std::min(b_norm, 1.0);
-                if (res_norm >= res_prev)
-                {
-                    ls.set_rhs(x);
-                    return LinsolReturnFlag::ITREF_INCREASE;
-                }
-                else
-                {
-                    // "accept" x_
-                    veccp(m_, x_, 0, x, 0);
-                }
-                if (res_norm < tol_ && i >= min_it_ref)
-                {
-                    ls.set_rhs(x);
-                    return LinsolReturnFlag::SUCCESS;
-                }
-                if (i == max_it_ref - 1)
-                {
-                    ls.set_rhs(x);
-                    return LinsolReturnFlag::ITREF_MAX_ITER;
-                }
-                ls.set_rhs(residual_);
-                // solve
-                solve_rhs(ls, x_);
-                // update x
-                axpy(m_, 1.0, x_, 0, x, 0, x_, 0);
-                res_prev = res_norm;
-            }
-            ls.set_rhs(x);
-            return LinsolReturnFlag::UNKNOWN;
-        }
+        LinsolReturnFlag solve_in_place(LinearSystem<LsType> &ls);
 
     protected:
         const Index m_;
---
+++ include/fatrop/linear_algebra/linear_solver.hxx
@@ -0,0 +1,82 @@
+//
+// Copyright (c) 2024 Lander Vanroye, KU Leuven
+//
+
+#ifndef __fatrop_linear_algebra_linear_solver_hxx__
+#define __fatrop_linear_algebra_linear_solver_hxx__
+
+#include "linear_solver.hpp"
+#include "fatrop/linear_algebra/linear_system.hpp"
+#include "fatrop/linear_algebra/linear_algebra.hpp"
+
+namespace fatrop
+{
+    template <typename Derived, typename LsType>
+    LinsolReturnFlag LinearSolver<Derived, LsType>::solve_once(LinearSystem<LsType> &ls, VecRealView &x_in)
+    {
+        return derived().solve_once_impl(ls, x_in);
+    }
+
+    template <typename Derived, typename LsType>
+    void LinearSolver<Derived, LsType>::solve_rhs(LinearSystem<LsType> &ls, VecRealView &x_in)
+    {
+        return derived().solve_rhs_impl(ls, x_in);
+    }
+
+    template <typename Derived, typename LsType>
+    LinsolReturnFlag LinearSolver<Derived, LsType>::solve_in_place(LinearSystem<LsType> &ls)
+    {
+        // reset x
+        vecse(m_, 0.0, x_, 0);
+        // solve
+        LinsolReturnFlag ret = solve_once(ls, x_);
+        if (ret != LinsolReturnFlag::SUCCESS)
+        {
+            return ret;
+        }
+        // reset residual
+        vecse(m_, 0.0, residual_, 0);
+        // "accept" x_
+        veccp(m_, x_, 0, x, 0);
+        Scalar res_prev = std::numeric_limits<Scalar>::max();
+        ls.get_rhs(tmp_);
+        const Scalar b_norm = norm_inf(tmp_);
+        for (Index i = 0; i < max_it_ref; i++)
+        {
+            // compute residual residual = Ax + b
+            ls.apply_on_right(x_, 1.0, tmp_, residual_);
+            // compute residual inf norm
+            const Scalar res_norm = norm_inf(residual_) / std::min(b_norm, 1.0);
+            if (res_norm >= res_prev)
+            {
+                ls.set_rhs(x);
+                return LinsolReturnFlag::ITREF_INCREASE;
+            }
+            else
+            {
+                // "accept" x_
+                veccp(m_, x_, 0, x, 0);
+            }
+            if (res_norm < tol_ && i >= min_it_ref)
+            {
+                ls.set_rhs(x);
+                return LinsolReturnFlag::SUCCESS;
+            }
+            if (i == max_it_ref - 1)
+            {
+                ls.set_rhs(x);
+                return LinsolReturnFlag::ITREF_MAX_ITER;
+            }
+            ls.set_rhs(residual_);
+            // solve
+            solve_rhs(ls, x_);
+            // update x
+            axpy(m_, 1.0, x_, 0, x, 0, x_, 0);
+            res_prev = res_norm;
+        }
+        ls.set_rhs(x);
+        return LinsolReturnFlag::UNKNOWN;
+    }
+} // namespace fatrop
+
+#endif // __fatrop_linear_algebra_linear_solver_hxx__
